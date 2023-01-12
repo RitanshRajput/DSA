18539aabbdf93a579dd6ad59ccd98f802f3b2550
RitanshRajput <it2042@global.org.in>
2023-01-12 23:17:39 +0530

    notes

---
+++ topic.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout<<"Namaste Duniya..." <<endl ;
+    
+}
+
+
+// #include<iostream>   = this is input/output library for the code to understand syntaxes
+// using namespace std;  = this is for certain object to works like cout / cin
+// int main() = this is our main function within which we will write all our code
+// cout<<     = this is used to print anything on the  output screen 
+// << endl;   = for newline
+// <<'\n';    = for newline
+// "hello.\n" =here also \n will print newline or move cursor to new line
+// << '\n';   = newline 
+// ;          = semi-colon tells that the line is completed/ended to the compiler.
+
+
+
---
+++ topic1.cpp
@@ -0,0 +1,36 @@
+     /********** 🔴🔴 Flowchart   */
+
+//🔸 FLowchart is Diagramatical way to describe the problem statement or a situation before making it into code.
+//🔸basic components in flowchart:
+//1. Problem
+//2.start point/task
+//3.input/output condition
+//4.process involve
+//5.decision to be made
+//6.End point/result
+
+
+
+//🔘 Components In flowchart:
+ 
+//1. (    )   = start/end          (ellipse)
+//2. /      /  =Input/output/print    (read a, b)   (parallelogram)
+//3.  |     |  =process/calculation  (i= i+1 )      (rectangle)
+//4.  🔷      =decision making /condition making     (if no, if yes )   (Diamond)
+//5. ⬆️ ⬇️   =to show flow direction/ connecting components
+//6.  🔘    =connector   (circle)
+
+
+
+
+//🔘 Psuedo code  (psuedo means Nakli ):
+
+//🔸Generic Way of representing code/logic/flow to slove the problem/approach
+
+//ex: psudo code for Sum of 2 no.
+// => Read a
+// => Read b
+// => Sum = a+b
+// => print sum
+
+
