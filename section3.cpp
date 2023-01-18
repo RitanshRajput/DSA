7486c21556c47a4da87cd7f976534bec4899fb80
RitanshRajput <it2042@global.org.in>
2023-01-19 02:31:41 +0530

    day7

--- section3.cpp
+++ section3.cpp
@@ -1,4 +1,4 @@
-//🔴🔴  Operators && For Loop
+                    //🔴🔴  Operators && For Loop
 
 
 //🔘Bitwise Operator :
@@ -89,7 +89,7 @@
 //  OUTPUT =>   1 1 0
 
 
-               /**** 🔴 Implementation ***/
+                  /**** 🔴 Implementation ***/
 
 // #include<iostream>
 // using namespace std ;
@@ -180,7 +180,7 @@
 // }
 
 
-//🔘🔘 Increment and Decrement :
+               //🔴🔴 Increment and Decrement :
 
 //🔸 Two increments are there post-increment( i++ / i+=1 ) and pre-increment( ++i )
 //🔸 Two decrements are there post-decrement( i-- / i-=1 ) and pre-decrement( --i )
@@ -265,7 +265,8 @@
 //  }
 
 
-//🔘🔘 For Loop :
+               //🔴🔴 For Loop :
+
 
 // for loop takes 3 argument (initialization ; condition ; updation)
 // separated by semicolon ; important .
@@ -455,7 +456,8 @@
 // }  
 
 
-//🔴🔴 Decimals and Binary :
+                   //🔴🔴 Decimals and Binary :
+
 
 //   Decimal    |     Binary
 //   ----------------------
@@ -705,19 +707,504 @@
 //         cout<<false;
 // }
 
-//❓ Question leetcode easy 5️⃣ ;
 
+
+                     //🔴🔴 Switch Statements and Functions :
+
+//🔸 When we need to add multiple condition what if we have many conditions then it'll be too
+//    hectic to Use for Loop , Thats where we use Switch Statement .
+
+//Keywords and expression :
+
+// switch (expression)
+// {
+// case const :  
+//       break;
+// case const :
+//       break;
+// case const :
+//       break;
+// default:
+//       break;
+// }
+
+//1. switch is a keyword which takes a any integral expression in bracket
+//2. case means when we have many condition just like that we have case here (i.e: case 1, case 2, case n)
+//3. const means constant it could be, Int / Char .. But it cannot be float and string value.
+//4. break stop the loop if the case meets the expected value then break statement stop the loop there, if not then loop will keep going on.
+//   if we dont use the break statement then even if the case is met still it will run till it finds break of it will end the loop.
+//5. default means if non of the above then we use default. it is not mandatory. 
+
+//Ex:
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+// int main() {
+//   int num = 2;
+//   char ch = '1' ;
+//   cout<<endl ;
+//   switch (ch){
+//       case 1: cout<<"First"<<endl;
+//             break;
+//       case '1': cout<<"Character 1"<<endl;
+//             break;
+//       case 2: cout<<"Second"<<endl;
+//             break;
+//       case 3: cout<<"third"<<endl;
+//             break;
+//       default: cout << "None of the Above"<<endl ;
+//   }
+// }
+
+//🔸Can we do nesting of switch , Yes!
+//Ex:
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+// int main() {
+//   int num = 1;
+//   char ch = '1' ;
+//   cout<<endl ;
+//   switch (ch){
+//       case 1: cout<<"First"<<endl;
+//             break;
+//       case '1': switch(num) {
+//                 case 1: cout<<"Nesting In swtich is happening"<<endl;
+//                 break;
+//                }
+//            break;
+
+//       case 2: cout<<"Second"<<endl;
+//             break;
+//       case 3: cout<<"third"<<endl;
+//             break;
+//       default: cout << "None of the Above"<<endl ;
+//   }
+// }
+
+//❓what if we use a switch statement inside a infinite while looop how can we get out of the loop ?
+// remember In this case we can only use break inside switch statement not in while loop;
+// => Ex:
 
 // #include<iostream>
 // #include<math.h>
 // using namespace std;
 // int main() {
+//   int n = 1;
+//   while(n <10 ){
+//       int ch = '1' ;
+//       switch(ch) {
+//           case 1: cout<<"Not 1"<<endl ;
+//           break;
+//           case '1':cout<<"Found 1"<<endl ;
+//           break;
+//       }
+//       cout<<"while looop"<<endl ;
+//      n-- ;
+//      exit(ch) ;
+//   }
+// }
+
+//🔸Answer=> we can use exit() statement inside exit() pass the name of the variable on which the loop is running.
+//           it will run the loop and get out.
+
+//❓ Question :
+//🔸 Create a calculator using switch case :
+
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+// int main() {
+//  int a, b ;
+//  cout<<"Enter value of A"<<endl ; ;
+//  cin>>a ;
+//  cout<<"Enter value of B"<<endl ; ;
+//  cin>>b ;
+
+//  char op ;
+//   cout<<"Enter operation you want to perform(+, -, *, /, %)"<<endl ; ;
+//   cin>>op ;
+ 
+// switch (op){
+//  case '+' : cout<<"answer : "<< a+b <<endl ;
+//  break ;
+//  case '-' : cout<<"answer : "<< a-b <<endl ;
+//  break ;
+//  case '*' : cout<<"answer : "<< a*b <<endl ;
+//  break ;
+//  case '/' : cout<<"answer : "<< a/b <<endl ;
+//  break ;
+//  case '%' : cout<<"answer : "<< a%b <<endl ;
+//  break ;
+//  default : cout<<"Operation no valid"<<endl ;
+//  break ;
+// }
+
+// }
+
+//❓ Question :
+//  USing switch case create a program that tells 
+//  how many notes of 100, notes of 50, notes of 20, notes of 1 will the total amount will have ?
+//explanation:
+// if total amount = 1330 ;
+// then 1330/100 = 13 notes of 100 , remaining 30
+// then 30/50 = 0 
+// then 30/20= 1 
+// then 10/1 =10
+
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+// int main() {
+  
+//   int total = 1330 ;
+//   int n100, n50, n20, n1 ;
+//   int remain = 0;
+//   switch (1){
+//       case 1 :{
+//             n100 = total / 100;
+//             cout<<"100 notes : "<<n100<<endl ;
+//             remain = total % 100 ;
+//             cout<<"remain :"<<remain<<endl ;
+           
+//            n50 = remain / 50 ;
+//            cout<<"50 notes : "<<n50<<endl ;
+//            remain = remain % 50 ;
+          
+//            n20 = remain / 20 ;
+//            cout<<"20 notes : "<<n20<<endl ;
+//            remain = remain % 20 ;
+
+//             n1 = remain / 1 ;
+//            cout<<"1 notes : "<<n1<<endl ;
+//            remain = remain % 1 ;
+//       }
+//       break ;
+//   }
+// }
+
+
+                   //🔴🔴 FUNCTIONS :
+//🔸when we want to use a collection of code to do a task again and again , we cannot do it again and again
+//  it will become bulky , take much space, make the program slow, and cause of bug
+//🔸Thats why we Use function : 
+// function is a program which does a well define task it takes value(parameter) as an input and perform the task on those input and gives/return output
+// Uses : define once and use again and again by just calling its name and give some input value and you get your answer.
+// we declare it above int main(){}
+
+
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// int power(int a, int b){          
+//       int ans = 1;
+
+//       for(int i=1; i<=b; i++){
+//             ans = ans * a;
+//       }
+//       return ans;
+// }
+
+//  int main(){
+//       int a , b ;
+//       cout<<"enter value of a and b"<<endl;
+//       cin>>a>>b ;
+
+//       int answer = power(a,b);
+//       cout<<"Answer : "<<answer<<endl ;
+//       return 0;
+//  }
+
+//🔸 FUnction cannot a variable declare in main function(int main(){ }) 
+
+//🔸❓ function decribes is no is even or odd .
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// //rerturn 1 => Even 
+// //return 0 => odd
+// bool isEven(int a) {
+//       if(a&1){
+//             return 0;
+//       }
+//       return 1;
+// }
+
+// int main() {
+//   int num; 
+//   cout<<"enter a number" <<endl;
+//   cin>>num ;
+
+//   if (isEven(num)){
+//    cout<<num<<" is Even "<<endl ;
+//   }
+//   else {
+//       cout<<num<<" is Odd  "<<endl ;
+//   }
+//   return 0 ;
+// }
+
+
+//❓ Question :
+// find  nCr = n! / r! * (n-r)!
+ 
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// int factorial(int n){                            //(int n int r) => function argument(means names listed in the function's definition)
+//       int fact = 1 ;
+//       for(int i=1; i<=n; i++){
+//             fact = fact * i ;
+//       }
+//       return fact ;
+// }
+
+// int nCr(int n, int r){                         //(int n int r) => function argument(means names listed in the function's definition)
+//       int nume = factorial(n) ;
+//       int deno = factorial(r) * factorial(n-r) ;
+//       return nume/deno ;
+// }
+
+// int main() {
+//    int n, r ;
+//    cout<<"enter value of n and r "<<endl;
+//    cin>>n>>r ;
+    
+//     int answer = nCr(n, r) ;              //(n, r) => function argument(means real value that is passed in function)
+//     cout<<"nCr of "<<n<<" and "<<r<<" is :" <<answer<<endl;
+//     return 0 ;
+// }
+
+//🔸 we use Void as a return_type in place of Int/char/float for a function when the function is not returning any value.
+
+//❓ Question :
+// Print counting using a function :
+
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// //function signature :
+// void printCounting(int n){        // (int n) => function paramater 
+
+//       //function body:
+//       for(int i=1; i<=n; i++){
+//             cout<< i <<" ";
+//       }
+//       cout<<endl ;
+// }
+
+// int main() {
+//  int n ;
+//  cout<<"enter a number"<<endl ;
+//  cin>>n ;
+
+// //function call :
+// printCounting(n) ;
+
+// return 0;
+// }
+
+//❓ Question :
+// No is prime or not using function :
+
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// // 1=> prime no:
+// // 0=> not a prime no:
+// bool isPrime(int n){
+//       for(int i=2; i<n; i++){
+//             if(n%i==0){
+//                   return 0;
+//             }
+//       }
+//       return 1;
+// }
+
+// int main() {
+// int n ;
+// cout<<"enter a no."<<endl;
+// cin>>n ;
+
+//  if (isPrime(n)){
+//       cout<<n<<" is prime no."<<endl ;
+//  }
+//  else{
+//       cout<<n<<" is Not a prime no."<<endl; 
+//  }
+
+// return 0 ;
+// }
+
+//❓ Question:
+// find Arithmetic progresiion: for(3 * n + 7) ;
+
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+// int ap(int n){
+//       int ans = (3*n+7) ;
+//       return ans ;
+// }
+// int main() {
+// int n ;
+// cout<<"enter no"<<endl;
+// cin>>n ;
+
+// int answer = ap(n);
+// cout<<"A.P for "<<n<<" is "<<answer<<endl ;
+
+// return 0;
+// }
+
+
+
+//❓ Question:
+// we have a and b give total number of there set bits(1) ;
+//explanation : 
+// a = 2 => 10 =>bits
+// b = 3 => 11 =>bits
+//setBits => 1 + 11 => 3 
+
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// int setBit(int a, int b){
+//       int ans = 0 ;
+//       int answer = 0;
+//       int bit = 0;
+//       while(a!=0){ 
+//            ans = ans + (a&1)  ;
+//            a = a>>1;
+//       }
+//         while(b!=0){
+//            answer = answer + (b&1 ) ;
+//            b = b>>1;
+//       }
+//       return ans + answer ;
+// }
+
+// int main() {
+// int a;
+// int b;
+//   cout<<"enter value of a\n" ;
+//   cin>>a;
+//   cout<<"enter value of b\n" ;
+//   cin>>b;
+
+//   int answer = setBit(a, b) ;
+//   cout<<"total setbit of "<<a<<"and"<<b<<" is :"<<answer<<endl;
+
+// return 0;
+// }
+
+//❓ Question:
+// fibonacci series :
+
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// int fibo(int n){
+// int temp =0;
+// int a = 1;
+// cout<<0<<endl<<1<<endl ;
+//    for(int i=0; i<n-2;i++){
+//         int next = temp + a;
+//         cout<<next<<endl ;
+//         temp = a;
+//         a = next ;
+//    }
+//    return 0;
+// }
+
+// int main() {
 // int n ;
 //   cout<<"enter number\n" ;
 //   cin>>n;
 
+//   int answer = fibo(n) ;
 
+//   return 0 ;
+// }
+
+
+                //🔴🔴 Pass by Value :
+//when we send a value from main function to a function it does not use the original value it creates a copy of that value and then perform task on it.
+
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// void pass(int n){
+//       n++ ;
+//       cout<<"From void pass : "<<n<<endl; 
+//       return ;
+// }
+
+// int main() {
+// int n ;
+//   cout<<"enter number\n" ;
+//   cin>>n;
+
+//   pass(n) ;
+//   cout<<"From main : "<<n<<endl; 
+
+// return 0 ;
+// }
+
+// //❓ Question output:
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// void update(int a){
+//       a = a/2 ;
+// }
+// int main(){
+//       int a = 10;
+//       update(a) ;
+  
+//   cout<<a<<endl ;                ///output: 10
 
 // }
 
-// 8/149
+//❓ Question output:
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// int update(int a){
+//       a-=5;
+//       return a;
+// }
+// int main(){
+// int a = 15;
+// update(a);
+// cout<<a<<endl ;                   //output: 15
+
+// }
+
+//❓ Question output:
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// int update(int a){
+//       int ans = a*a;
+//       return ans ;
+// }
+// int main(){
+// int a = 14;
+// a = update(a) ;
+// cout<<a<<endl ;                   //output: 196
+
+// }
+
+
