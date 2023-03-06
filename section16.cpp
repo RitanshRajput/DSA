37369e85ff361873cb2ab71cd6a846609c08b1f1
Ritansh Rajput <it2042@global.org.in>
2023-03-07 00:46:11 +0530

    day43

--- section16.cpp
+++ section16.cpp
@@ -1670,4 +1670,61 @@
 //     return 0;
 // }
 
-//41/149
+
+
+//❓ Question : Leetcode Medium
+// Given an integer array nums of unique elements, return all possible subsets (the power set).
+// The solution set must not contain duplicate subsets. Return the solution in any order.
+
+// Example 1:
+// Input: nums = [1,2,3]
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+// Example 2:
+// Input: nums = [0]
+// Output: [[],[0]]
+ 
+// Constraints:
+// 1 <= nums.length <= 10
+// -10 <= nums[i] <= 10
+// All the numbers of nums are unique.
+
+// Power set : set of all subsets
+// formula : no. of element in power set : 2^n  
+
+// #include<iostream>
+// #include<vector>
+// using namespace std ;
+
+// void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>> &ans){
+    
+//     //base case
+//     if(index >= nums.size()){
+//         ans.push_back(output) ;
+//         return ;
+//     }
+
+//     //exclude
+//     solve(nums, output, index+1, ans);
+
+//     //include
+//     int element = nums[index] ;
+//     output.push_back(element) ;
+//     solve(nums, output, index+1 , ans) ;
+
+// }
+
+// int main()
+// {
+//     vector<int> nums = { 1, 2, 2 };
+//     vector<vector<int>> ans ;
+//     vector<int> output ;
+//     int index = 0 ;
+
+//     solve(nums, output, index, ans) ;
+    
+//     return 0 ;
+// }
+
+
+//42/149
