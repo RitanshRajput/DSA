71401ba17b7ca7a21177307d2ad1b83a519c2536
Ritansh Rajput <it2042@global.org.in>
2023-03-09 02:27:02 +0530

    day44

--- section16.cpp
+++ section16.cpp
@@ -1716,7 +1716,10 @@
 
 // int main()
 // {
-//     vector<int> nums = { 1, 2, 2 };
+//     vector<int> nums ;
+//     nums.push_back(1) ;
+//     nums.push_back(2) ;
+//     nums.push_back(3) ;
 //     vector<vector<int>> ans ;
 //     vector<int> output ;
 //     int index = 0 ;
@@ -1726,5 +1729,95 @@
 //     return 0 ;
 // }
 
+                                        //🔴Basic Backtracking question
 
-//🔴❓ Phone KeyPad Recurssion : USing Recurssion :
+//🔴❓Question : Leetcode Medium : 
+// Asked in company :  FAAG , Uber, adobe, twitter, bloomberg, goldman sachs, 
+//                     oracle, nutanix, duolingo ,snapchat , samsung, infuit, 
+//                     epic systems, swiggy
+
+//🔴 Phone KeyPad Recurssion : USing Recurssion 
+
+// Given a string containing digits from 2-9 inclusive, 
+// return all possible letter combinations that the number could represent.
+// Return the answer in any order.
+
+// A mapping of digits to letters (just like on the telephone buttons) is given below.
+// Note that 1 does not map to any letters.
+
+//  1  2  3         0_0  abc  def
+//  4  5  6         ghi  jkl  mno
+//  7  8  9         pqrs tuv  wxyz
+//     0             *    -    #
+
+// Example 1:
+// Input: digits = "23"
+// Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+// Example 2:
+// Input: digits = ""
+// Output: []
+
+// Example 3:
+// Input: digits = "2"
+// Output: ["a","b","c"]
+ 
+// Constraints:
+// 0 <= digits.length <= 4
+// digits[i] is a digit in the range ['2', '9'].
+
+// #include<iostream>
+// #include<vector>
+// using namespace std ;
+
+// void solve(string digit, string output, int index, vector<string>& ans, string mapping[]) {
+
+//     //base case 
+//     if(index >= digit.length()){
+//         ans.push_back(output) ;
+//         return ;
+//     }
+
+//     //processing 
+//     int number = digit[index] - '0' ;         // to convert string into integer - '0' ,index = 1 , digit[1] = 2 ==> 2 - '0' ==> 2 (else it will give ascii number)
+//     string value = mapping[number] ;          // ex: value  = mapping[number] , number = 2 , mapping[2] = "abc" 
+
+//     for(int i=0; i<value.length(); i++){
+
+//         output.push_back(value[i]) ;
+//         //recursive call
+//         solve(digit, output, index+1, ans, mapping) ;
+        
+//         output.pop_back() ;
+//     }
+// }
+
+
+// vector<string> letterCombination(string digit) {
+         
+//          vector<string> ans ;
+//          if(digit.length() == 0){
+//             return ans ;
+//          }
+//          string output ;
+//          int index = 0;
+//          string mapping[10] = {"","","abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv","wxyz"} ;
+//          solve(digit, output, index, ans, mapping) ;
+   
+//          return ans ;
+// }
+
+// int main() {
+
+// string digit = "23" ;
+
+// vector<string> res = letterCombination(digit) ;
+
+// for(int i=0; i<res.size(); i++){
+//    cout<< res[i] <<" " ;
+// }
+
+//     return 0;
+// }
+
+
