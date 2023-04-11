d1cb38b3a53eb08826d39b0c5cf3f1a429087891
Ritansh Rajput <it2042@global.org.in>
2023-04-11 18:24:52 +0530

    day60

--- zPractice.cpp
+++ zPractice.cpp
@@ -714,3 +714,67 @@
 
 //     return 0;
 // }
+
+
+//                     ❓ Minimum number of jumps 
+//Given an array of N integers arr[] where each element represents the maximum length of the jump 
+//that can be made forward from that element. 
+//This means if arr[i] = x, then we can jump any distance y such that y ≤ x.
+
+// Find the minimum number of jumps to reach the end of the array (starting from the first element).
+// If an element is 0, then you cannot move through that element.
+
+// Note: Return -1 if you can't reach the end of the array.
+
+// Example 1:
+// Input:
+// N = 11 
+// arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9} 
+// Output: 3 
+// Explanation: 
+// First jump from 1st element to 2nd 
+// element with value 3. Now, from here 
+// we jump to 5th element with value 9, 
+// and from here we will jump to the last. 
+
+// Example 2:
+// Input :
+// N = 6
+// arr = {1, 4, 3, 2, 6, 7}
+// Output: 2 
+// Explanation: 
+// First we jump from the 1st to 2nd element 
+// and then jump to the last element.
+
+// #include<iostream>
+// using namespace std;
+
+// int minJump(int arr[], int n) {
+
+//     int maxDistance = 0 ;
+//     int count = 0;
+//     int currentElem = 0 ;
+
+//     for(int i=0; i<n-1; i++){
+//         maxDistance = max(maxDistance, i+arr[i]) ;
+
+//         if(i == currentElem) {
+//             count++ ;
+//             currentElem = maxDistance ;
+//            if(currentElem >= n-1) {
+//             return count ; 
+//            }
+//         }
+//     }
+//      return -1 ;
+// }
+
+// int main() {
+//     int arr[11] =  {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9} ;
+//     int n = sizeof(arr) / sizeof(arr[0]) ;
+    
+//     int ans = minJump(arr, n) ;
+//     cout<<" Minimum jumps  : "<< ans <<endl ;
+    
+// return 0;
+// }
