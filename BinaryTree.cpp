535cdc28b991776fd2c7b102e46b7e8b5afd093d
Ritansh Rajput <it2042@global.org.in>
2023-05-05 21:07:19 +0530

    day82

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -2859,4 +2859,185 @@
 //🔴Time complexity: O(N^2)
 //🔴space complexity: O(H)
 
-//73/149
+
+
+// 🔴🔴🔴                  //❓Question: Burning Tree
+
+// Given a binary tree and a node data called target.
+//  Find the minimum time required to burn the complete binary tree if the target is set on fire.
+//  It is known that in 1 second all nodes connected to a given node get burned.
+/// That is its left child, right child, and parent.
+// Note: The tree contains unique values.
+
+// Example 1:
+// Input:      
+//           1
+//         /   \
+//       2      3
+//     /  \      \
+//    4    5      6
+//        / \      \
+//       7   8      9
+//                    \
+//                    10
+// Target Node = 8
+// Output: 7
+// Explanation: If leaf with the value 
+// 8 is set on fire. 
+// After 1 sec: 5 is set on fire.
+// After 2 sec: 2, 7 are set to fire.
+// After 3 sec: 4, 1 are set to fire.
+// After 4 sec: 3 is set to fire.
+// After 5 sec: 6 is set to fire.
+// After 6 sec: 9 is set to fire.
+// After 7 sec: 10 is set to fire.
+// It takes 7s to burn the complete tree.
+
+// Example 2:
+// Input:      
+//           1
+//         /   \
+//       2      3
+//     /  \      \
+//    4    5      7
+//   /    / 
+//  8    10
+// Target Node = 10
+// Output: 5
+
+//🔴Approach:
+//step1: 
+//nodeToParent mapping (to find the parent of current node)
+// Map<Node*, Node*> nodeToParent mapping
+// level order traversal 
+
+//step2: 
+// find target node (from which we want to start burning)
+// time complexity: o(H)
+
+//step 3: 
+// start burning
+// take 2 data structure: 
+// dataStructure 1: using map<Node, bool> and queue check if node visited or not
+// dataStructure 2:  traverse target node
+// check if there are any left, right child and parent present or not to burn them
+
+// if visited node then ignore else mark visited and add in queue 
+// check if there was any added node in queue or not ( if true then === time++) (if false then == ignore)
+
+// #include<iostream>
+// #include<map>
+// #include<queue>
+// using namespace std;
+
+// class Node{
+//     public:
+//     int data ;
+//     Node* left; 
+//     Node* right;
+
+//     Node(int d) {
+//         this -> data = d ;
+//         this -> left = NULL ;
+//         this -> right = NULL ;
+//     }
+// };
+
+// //create parent mapping function
+// //which also returns the parent node
+// Node* createParentMapping(Node* root, int target, map<Node*, Node*> &nodeToParent) {
+      
+//     Node* result = NULL ;
+
+//     queue<Node*> q ;
+//     q.push(root) ;
+//     nodeToParent[root] = NULL ;          //root doesnot have a parent so we consider NULL as its parents
+
+// // level order traversal
+//     while(!q.empty()) {
+//         Node* front = q.front() ;
+//         q.pop() ;
+
+//         if(front -> data == target) {
+//             result = front;
+//         }
+
+//         if(front -> left){
+//             nodeToParent[front -> left] = front ;
+//             q.push(front -> left) ;
+//         }
+//         if(front -> right){
+//             nodeToParent[front -> right] = front;
+//             q.push(front -> right) ;
+//         }
+//     }
+
+//     return result ;
+// }
+
+// //burn Time function
+// int burnTree(Node* targetNode , map<Node*, Node*> & nodeToParent) {
+       
+//     map<Node*, bool> visited ;
+//     queue<Node*> q ;
+
+//     q.push(targetNode);
+//     visited[targetNode] = true ;
+
+//     int ans = 0 ;
+
+//     while( !q.empty()) {
+
+//         bool flag = 0 ;
+//         int size = q.size() ;
+
+//         for(int i=0; i<size; i++){
+//             //process neighbouring nodes
+//             Node* front = q.front() ;
+//             q.pop() ;
+
+//             if(front -> left && !visited[front -> left]) {
+//                 flag = 1 ;                          // 1 == true
+//                 q.push(front -> left) ;
+//                 visited[front -> left] = 1;         // 1 == true
+//             }
+
+//             if(front -> right && !visited[front -> right]) {
+//                 flag = 1 ;                          // 1 == true
+//                 q.push(front -> right) ;
+//                 visited[front -> right] = 1;        // 1 == true
+//             }
+             
+//             if(nodeToParent[front] && !visited[nodeToParent[front]]) {
+//                 flag = 1 ;                          // 1 == true
+//                 q.push(nodeToParent[front]);
+//                 visited[nodeToParent[front]] = 1;   // 1 == true
+//             }
+//         }
+
+//         if(flag == 1) {
+//             ans++ ;
+//         }
+//     }
+   
+//    return ans ;
+// }
+
+// //main function
+// int minTime(Node* root ,int target) {
+//     //step1: create nodeToParent mapping
+//     //step2: find target node
+//     //step3: burn the tree in min time 
+
+//     map<Node*, Node*> nodeToParent ;
+//     Node* targetNode = createParentMapping(root, target, nodeToParent) ;
+
+//     int ans = burnTree(targetNode, nodeToParent) ; 
+
+//     return ans ;
+// }
+//🔴Time complexity: O(N)      //O(N)LOT + O(N)burntree         //lot level order traversal
+//🔴space complexity: O(N)
+
+
+//74/149
