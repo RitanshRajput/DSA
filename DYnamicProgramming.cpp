804c33e6c0fedeee9c4d52a16efaf13800532812
Ritansh Rajput <it2042@global.org.in>
2023-06-07 22:27:04 +0530

    day104

--- DYnamicProgramming.cpp
+++ DYnamicProgramming.cpp
@@ -431,4 +431,67 @@
 //🔸time complexity: O(n)
 //🔸space complexity: O(1)
 
-// 113 / 149
+
+
+//                   //❓Question: Minimum elements
+
+// You are given an array of 'N' distinct integers and an integer 'X' representing the target sum. You have to tell the minimum number of
+//  elements you have to take to reach the target sum 'X'.
+//  Note:
+//    You have an infinite number of elements of each type.
+
+// For Example
+//    If N=3 and X=7 and array elements are [1,2,3].
+//    Way 1 - You can take 4 elements [2, 2, 2, 1] as 2 + 2 + 2 + 1 = 7.
+//    Way 2 - You can take 3 elements [3, 3, 1] as 3 + 3 +1=7.
+//    Here, you can see in Way 2 we have used 3 coins to reach the target sum of 7.
+//    Hence the output is 3.
+
+// Constraints:
+//    1 <= T <= 10
+//      <= N <= 15
+//    1 <= nums[i] <= (2^31) - 1
+//    1 <= x <= 10000
+//    All the elements of the "nums" array will be unique.
+//    Time limit: 1 sec
+
+// Sample Input 1 :
+// 2
+// 3 7
+// 1 2 3
+// 1 0
+// 1
+// Sample Output 1 :
+//  3
+//  0
+// Explanation For Sample Output 1:
+// For the first test case,
+// Way 1 - You can take 4 elements  [2, 2, 2, 1] as 2 + 2 + 2 + 1 = 7.
+// Way 2 - You can take 3 elements  [3, 3, 1] as 3 + 3 + 1 = 7.
+// Here, you can see in Way 2 we have used 3 coins to reach the target sum of 7.
+// Hence the output is 3.
+
+// For the second test case,
+// Way 1 - You can take 3 elements  [1, 1, 1] as 1 + 1 + 1  = 3.
+// Way 2 - You can take 2 elements  [2, 1] as 2 + 1 = 3.
+// Here, you can see in Way 2 we have used 2 coins to reach the target sum of 7.
+// Hence the output is 2.
+// Sample Input 2 :
+// 2
+// 3 4
+// 12 1 3
+// 2 11
+// 2 1
+// Sample Output 2 :
+// 2
+// 6 
+
+// #include <iostream>
+// using namespace std;
+
+// int minimumElements(vector<int> &num, int x)
+// {
+//     // Write your code here.
+// }
+
+//113 /149
