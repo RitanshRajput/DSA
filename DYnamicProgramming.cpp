77340ef4e6aebc3a4a3f8fc7659f19c9aac91f6c
Ritansh Rajput <it2042@global.org.in>
2023-06-08 01:24:27 +0530

    day105

--- DYnamicProgramming.cpp
+++ DYnamicProgramming.cpp
@@ -486,12 +486,134 @@
 // 2
 // 6 
 
+//🔴approach: recursion
+//🔸Time limit exceeded
+
 // #include <iostream>
+// #include<vector>
+// #include<limits.h> 
 // using namespace std;
 
-// int minimumElements(vector<int> &num, int x)
-// {
-//     // Write your code here.
+// int solve(vector<int> &num, int x) {
+//     //base case
+//     if(x == 0) {
+//         return 0 ;
+//     }
+
+//     if(x  < 0){
+//         return INT_MAX ;
+//     }
+
+//     int mini = INT_MIN ;
+
+//     for(int i=0; i<num.size(); i++) {
+//          int ans = solve(num, x - num[i]) ;
+//         if(ans != INT_MIN) {
+//             mini = min(mini, 1+ans) ;
+//         }
+//     }
+
+//     return mini ;
 // }
 
-//113 /149
+// int minimumElements(vector<int> &num, int x)
+// { 
+//   int ans = solve(num, x) ;    
+//   if( ans == INT_MAX) {
+//     return -1;
+//   }
+//  return ans ;
+// }
+//🔸time complexity: Exponential          // bad very bad
+
+
+// //🔴approach: recursion + memoization
+// //🔸 Time limit exceeded
+
+// #include <iostream>
+// #include<vector>
+// #include<limits.h> 
+// using namespace std;
+
+// int solve(vector<int> &num, int x, vector<int> &dp ) {
+//     //base case
+//     if(x == 0) {
+//         return 0 ;
+//     }
+
+//     if(x  < 0){
+//         return INT_MAX ;
+//     }
+
+//     if( dp[x] != -1) {
+//         return dp[x] ;
+//     }
+
+//     int mini = INT_MIN ;
+
+//     for(int i=0; i<num.size(); i++) {
+//          int ans = solve(num, x - num[i], dp) ;
+//         if(ans != INT_MIN) {
+//             mini = min(mini, 1+ans) ;
+//         }
+//     }
+//     dp[x] = mini ;
+
+//     return mini ;
+// }
+
+// int minimumElements(vector<int> &num, int x)
+// { 
+//   vector<int> dp(x + 1, -1) ;
+//   int ans = solve(num, x, dp) ;
+
+//   if(ans == INT_MAX) {
+//      return -1 ;
+//   }
+//   else{
+//      return ans ;
+//   }
+// }
+//🔸time complexity: O(x * n)             // x = amount, n = no of coins
+//🔸space complexity: O(x)
+
+
+//🔴approach: Tabulation 
+
+// #include <iostream>
+// #include<vector>
+// #include<limits.h> 
+// using namespace std;
+
+// int solve(vector<int> &num, int x) {
+
+//   vector<int> dp(x+1, INT_MAX) ;
+//   dp[0] = 0 ;
+
+//   for(int i=1; i<=x; i++){
+//     // trying to solve for every amount figure from 1 to x
+//     for(int j=0; j<num.size(); j++) {
+
+//         if( i-num[j] >= 0 && dp[i-num[j]] != INT_MAX){ 
+//            dp[i] = min(dp[i], 1 + dp[i-num[j]]) ;
+//         }
+//     }
+//   }
+
+//   if( dp[x] == INT_MAX) {
+//      return -1 ;
+//   }
+
+//   return dp[x] ;
+
+// }
+
+// int minimumElements(vector<int> &num, int x)
+// { 
+//   return solve(num, x) ;
+// }
+//🔸time complexity: O(N)
+//🔸space complexity: O(x)
+
+
+// 114 / 149
