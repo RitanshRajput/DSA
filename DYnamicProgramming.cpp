7f1b20722f151bac0d8efd548516a6c10f45be4b
Ritansh Rajput <it2042@global.org.in>
2023-06-10 01:43:23 +0530

    day108

--- DYnamicProgramming.cpp
+++ DYnamicProgramming.cpp
@@ -899,4 +899,206 @@
 //🔸time complexity: O(n)
 //🔸space complexity: O(n)
 
-//  116 / 149
+
+
+//🔴🔴🔴          //❓Question: cut into segments
+
+// You are given an integer 'N' denoting the length of the rod. You need to determine the maximum number of segments you can make of
+// this rod provided that each segment should be of the length 'X', 'Y', or 'Z'.
+
+// Sample Input 1:
+// 2
+// 7 5 2 2
+// 8 3 3 3
+// Sample Output 1:
+// 2
+// 0
+// Explanation For Sample Input 1:
+//                7
+//   |=====================|=========
+//   |       5             |  2
+//   |                     | 
+// 
+//    ====================
+//           5
+//                      ============
+//                           2
+//
+//
+// In the first test case, cut it into 2 parts of 5 and 2.
+
+// In the second case, there is no way to cut into segments of 3 length only as the length of the rod is less than the given length. 
+// Sample Input 2:
+// 2
+// 7 3 2 2
+// 8 1 4 4
+// Sample Output 2:
+// 3
+// 8
+// Explanation For Sample Input 2:
+// In the first test case, cut it into 3 parts of 3, 2 and 2.
+
+// In the second case, cut it into 8 parts of length 1.
+
+
+//🔴approach: Recursion
+//🔸time limit exceeded
+// #include<iostream>
+// #include<limits.h>
+// using namespace std;
+
+// int solve(int n, int x, int y, int z){
+//     //base case
+//     if(n == 0) {
+//         return 0 ;
+//     }
+
+//     if(n < 0){
+//         return INT_MIN ;
+//     }
+
+//     int a = solve(n-x, x, y, z) + 1 ;
+//     int b = solve(n-y, x, y, z) + 1 ;
+//     int c = solve(n-z, x, y, z) + 1 ;
+
+//     int ans = max(a, max(b, c)) ;
+//     return ans ;
+// }
+
+// int cutSegments(int n, int x, int y, int z) {
+
+//     int ans = solve(n, x, y, z) ;
+//     if( ans < 0){
+//         return 0 ;
+//     }
+//     else{
+//         return ans ;
+//     }
+// }
+//🔸time complexity: exponential
+//🔸space complexity: exponential
+
+
+// //🔴approach: Recursion + memoization
+
+// #include<iostream>
+// #include<limits.h>
+// #include<vector>
+// using namespace std;
+
+// int solve(int n, int x, int y, int z, vector<int> &dp){
+//     //base case
+//     if(n == 0) {
+//         return 0 ;
+//     }
+
+//     if(n < 0){
+//         return INT_MIN ;
+//     }
+//     //step3:
+//    if( dp[n] != -1) {
+//     return dp[n] ;
+//    }
+
+//     int a = solve(n-x, x, y, z, dp) + 1 ;
+//     int b = solve(n-y, x, y, z, dp) + 1 ;
+//     int c = solve(n-z, x, y, z, dp) + 1 ;
+//     //step2:
+//     dp[n] = max(a, max(b, c)) ;
+//     return dp[n] ;
+// }
+
+// int cutSegments(int n, int x, int y, int z) {
+//     //step1:
+//     vector<int> dp(n+1, -1) ;
+//     int ans = solve(n, x, y, z, dp) ;
+
+//     if(ans < 0){   
+//     return 0 ;
+//     }
+//     else{
+//         return ans ;
+//     }
+// }
+//🔸time complexity:  O(n)
+//🔸space complexity: O(n) + O(n)
+
+
+//🔴approach: Tabulation
+
+// #include<iostream>
+// #include<limits.h>
+// #include<vector>
+// using namespace std;
+
+// int solve(int n, int x, int y, int z){
+//       //step1:
+//     vector<int> dp(n+1, INT_MIN) ;
+//     dp[0] = 0 ;
+
+//     for(int i=1; i<=n; i++) {
+//         if( i-x >= 0)
+//         dp[i] = max(dp[i], dp[i-x]+1) ;
+
+//         if( i-y >= 0)
+//         dp[i] = max(dp[i], dp[i-y]+1) ;
+
+//         if( i-z >= 0)
+//         dp[i] = max(dp[i], dp[i-z]+1) ;
+//     }
+
+//     if( dp[n] < 0){
+//         return 0 ;
+//     }
+//     else{
+//         return dp[n] ;
+//     }
+// }
+
+// int cutSegments(int n, int x, int y, int z) {
+  
+//     return solve(n, x, y, z) ;
+// }
+//🔸time complexity: O(n)
+//🔸space complexity: O(n)
+
+//🔴approach: Tabulation (without INT_MIN)
+
+// #include<iostream>
+// #include<limits.h>
+// #include<vector>
+// using namespace std;
+
+// int solve(int n, int x, int y, int z){
+//       //step1:
+//     vector<int> dp(n+1, -1) ;
+//     dp[0] = 0 ;
+
+//     for(int i=1; i<=n; i++) {
+//         if( i-x >= 0 && dp[i-x] != -1)
+//         dp[i] = max(dp[i], dp[i-x]+1) ;
+
+//         if( i-y >= 0 && dp[i-y] != -1)
+//         dp[i] = max(dp[i], dp[i-y]+1) ;
+
+//         if( i-z >= 0 && dp[i-z] != -1)
+//         dp[i] = max(dp[i], dp[i-z]+1) ;
+//     }
+
+//     if( dp[n] < 0){
+//         return 0 ;
+//     }
+//     else{
+//         return dp[n] ;
+//     }
+// }
+
+// int cutSegments(int n, int x, int y, int z) {
+  
+//     return solve(n, x, y, z) ;
+// }
+//🔸time complexity: O(n)
+//🔸space complexity: O(n)
+
+
+// 117 / 149
