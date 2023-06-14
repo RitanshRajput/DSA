e15d2b925a71d626bba86fb4bff9867cab984695
Ritansh Rajput <it2042@global.org.in>
2023-06-14 23:10:28 +0530

    day111

--- DYnamicProgramming.cpp
+++ DYnamicProgramming.cpp
@@ -1905,4 +1905,35 @@
 
 
 
-//  121 / 149
+
+//🔴🔴🔴           //❓Question: Get Minimum Square
+
+// Given a number N. Find the minimum number of squares of any number that sums to N. 
+// For Example: If N = 100 , N can be expressed as (10*10) and also as (5*5 + 5*5 + 5*5 + 5*5)
+//  but the output will be 1 as minimum number of square is 1 , i.e (10*10).
+ 
+//  Constraints:
+// 1 <= N <= 10000
+
+// Example 1:
+// Input: N = 100
+// Output: 1
+// Explanation: 10 * 10 = 100
+
+
+// Example 2:
+// Input: N = 6
+// Output: 3
+// Explanation = 1 * 1 + 1 * 1 + 2 * 2 = 6
+
+//🔴approach:
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class solution{
+    public:
+    int minSquares(int n) {
+        
+    }
+};
