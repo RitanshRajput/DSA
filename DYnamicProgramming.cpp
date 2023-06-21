f1923267bd1f8f89653e4e9cd271ad37ac71a031
Ritansh Rajput <it2042@global.org.in>
2023-06-21 23:36:09 +0530

    day117

--- DYnamicProgramming.cpp
+++ DYnamicProgramming.cpp
@@ -2656,4 +2656,229 @@
 
 
 
-// 126 / 149
+//🔴🔴                //❓Question: 1824. Minimum Sideway Jumps  (HARD)
+
+// There is a 3 lane road of length n that consists of n + 1 points labeled from 0 to n. 
+// A frog starts at point 0 in the second lane and wants to jump to point n. 
+// However, there could be obstacles along the way.
+
+// You are given an array obstacles of length n + 1 where each obstacles[i] (ranging from 0 to 3)
+// describes an obstacle on the lane obstacles[i] at point i. 
+// If obstacles[i] == 0, there are no obstacles at point i. 
+// There will be at most one obstacle in the 3 lanes at each point.
+
+// For example, if obstacles[2] == 1, then there is an obstacle on lane 1 at point 2.
+// The frog can only travel from point i to point i + 1 on the same lane 
+// if there is not an obstacle on the lane at point i + 1. To avoid obstacles, 
+// the frog can also perform a side jump to jump to another lane (even if they are not adjacent)
+//  at the same point if there is no obstacle on the new lane.
+
+// For example, the frog can jump from lane 3 at point 3 to lane 1 at point 3.
+// Return the minimum number of side jumps the frog needs to reach any lane 
+// at point n starting from lane 2 at point 0.
+
+// Note: There will be no obstacles on points 0 and n.
+
+//🔸example 1: 
+// lane1  ___________________________________
+//           [stone]            ^______>_____>             
+// lane2  ----------------------|------------
+//         [frog] ----->|    [stone]         
+// lane3  --------------|       |------------
+//                      |______>|    [stone]
+//        _____________________________________
+// 0              1       2      3     4
+
+// Input: obstacles = [0,1,2,3,0]
+// Output: 2 
+// Explanation: The optimal solution is shown by the arrows above. 
+// There are 2 side jumps (red arrows).
+// Note that the frog can jump over obstacles only when making side jumps
+//  (as shown at point 2).
+
+//🔸exmaple 2:
+// lane1  ______________________________________________
+//                [stone]   [stone]
+// lane2  ----------------------------------------------
+//         [frog] ------>------->------>------------->
+// lane3  ----------------------------------------------
+//                                   [stone]  [stone]
+//       ________________________________________________
+// 0          1          2         3       4      5
+
+// Input: obstacles = [0,1,1,3,3,0]
+// Output: 0
+// Explanation: There are no obstacles on lane 2. No side jumps are required
+
+// Constraints:
+// obstacles.length == n + 1
+// 1 <= n <= 5 * 105
+// 0 <= obstacles[i] <= 3
+// obstacles[0] == obstacles[n] == 0
+
+//🔴appraoch : Recursion
+// TIME lIMIT EXCEEDED 
+// #include<iostream>
+// #include<vector>
+// #include<limits.h>
+// using namespace std;
+
+// int solve(vector<int> &obstacles, int currlane, int currpos) {
+//     int n = obstacles.size() - 1;
+//     //base case
+//     if(currpos == n) {
+//         return 0 ;
+//     }
+
+//     // if no obstacles in same lane (2 lane) where frog is starting from, go forwward
+//     if(obstacles[currpos + 1] != currlane) {
+//         return solve(obstacles, currlane, currpos+1) ;
+//     }
+//     else{
+//         // else go sideways 
+//         int ans = INT_MAX;                 // for case: if cannot go further but, can go both sideways, lane1 and lane3 as well 
+//         for(int i=1; i<=3; i++) {          //   then to decide which side to go we will go to the minimum side, ans = min
+//             if(currlane != i && obstacles[currpos] != i)
+//             ans = min(ans, 1+solve(obstacles, i, currpos)) ;
+//         }
+//         return ans;
+//     }
+// }
+
+// int minSidejumps(vector<int> &obstacles) {
+//     return solve(obstacles, 2, 0) ;
+// }
+//🔸time complexity: exponential
+//🔸space complexity: exponential
+
+
+//🔴appraoch : Recursion +Memoization
+// #include<iostream>
+// #include<vector>
+// #include<limits.h>
+// using namespace std;
+
+// int solve(vector<int> &obstacles, int currlane, int currpos, vector<vector<int>> &dp) {
+//     int n = obstacles.size() - 1;
+//     //base case
+//     if(currpos == n) {
+//         return 0 ;
+//     }
+
+//     if(dp[currlane][currpos] != -1){
+//         return dp[currlane][currpos] ;
+//     }
+
+//     if(obstacles[currpos + 1] != currlane) {
+//         return solve(obstacles, currlane, currpos+1, dp) ;
+//     }
+//     else{
+//         int ans = INT_MAX;                 
+//         for(int i=1; i<=3; i++) {          
+//             if(currlane != i && obstacles[currpos] != i)
+//             ans = min(ans, 1+solve(obstacles, i, currpos, dp)) ;
+//         }
+//         dp[currlane][currpos] = ans;
+//         return dp[currlane][currpos];
+//     }
+// }
+
+// int minSidejumps(vector<int> &obstacles) {
+//     int n = obstacles.size();
+//     vector<vector<int>> dp(4, vector<int>(n, -1)) ;
+
+//     return solve(obstacles, 2, 0, dp) ;
+// }
+//🔸time complexity: O(n)
+//🔸space complexity: O(4 * n)
+
+
+//🔴appraoch : Tabulation
+// #include<iostream>
+// #include<vector>
+// #include<limits.h>
+// using namespace std;
+
+// int solve(vector<int> &obstacles) {
+//     int n = obstacles.size() -1 ;
+//     vector<vector<int>> dp(4, vector<int>(obstacles.size(), 1e9)) ;  // to tackle integer overflow error
+    
+//     dp[0][n] = 0;
+//     dp[1][n] = 0;
+//     dp[2][n] = 0;
+//     dp[3][n] = 0;
+
+//     for(int currpos = n-1; currpos>=0; currpos--) {
+//         for(int currlane = 1; currlane<=3; currlane++) {
+
+//             if(obstacles[currpos + 1] != currlane) {
+//                 dp[currlane][currpos] = dp[currlane][currpos + 1] ;
+//             }
+//             else{
+//                 int ans = 1e9;                 // to tackle integer overflow error
+//                 for(int i=1; i<=3; i++){
+//                     if(currlane != i && obstacles[currpos] != i) {
+//                         ans = min(ans, 1 + dp[i][currpos + 1]) ;
+//                     }
+//                 }
+//                 dp[currlane][currpos] = ans ;
+//             }
+//         }
+//     }
+//        return min(dp[2][0], min(1+dp[1][0], 1+dp[3][0])) ;
+// }
+
+// int minSidejumps(vector<int> &obstacles) {
+//     return solve(obstacles) ;
+// }
+//🔸time complexity: O(n)                     // O(3 * 3*n)
+//🔸space complexity: O(4 * n)
+
+
+
+//🔴appraoch : space optimisation
+// #include<iostream>
+// #include<vector>
+// #include<limits.h>
+// using namespace std;
+
+// int solve(vector<int> &obstacles) {
+//     int n = obstacles.size() -1 ;
+    
+//     vector<int> curr(4, INT_MAX) ;
+//     vector<int> next(4, INT_MAX) ;
+
+//     next[0] = 0;
+//     next[1] = 0;
+//     next[2] = 0;
+//     next[3] = 0;
+
+//     for(int currpos = n-1; currpos>=0; currpos--) {
+//         for(int currlane = 1; currlane<=3; currlane++) {
+
+//             if(obstacles[currpos + 1] != currlane) {
+//                 curr[currlane] = next[currlane] ; 
+//             }
+//             else{
+//                 int ans = 1e9;                 // to tackle integer overflow error
+//                 for(int i=1; i<=3; i++){
+//                     if(currlane != i && obstacles[currpos] != i) {
+//                         ans = min(ans, 1 + next[i]) ;
+//                     }
+//                 }
+//                 curr[currlane] = ans ;
+//             }
+//         }
+//         next = curr;
+//     }
+//        return min(next[2], min(1+next[1], 1+next[3])) ;
+// }
+
+// int minSidejumps(vector<int> &obstacles) {
+//     return solve(obstacles) ;
+// }
+//🔸time complexity:  O(n)             // O(3 * 3*n)
+//🔸space complexity: O(1)               // O(4)
+
+
+// 127 / 149
