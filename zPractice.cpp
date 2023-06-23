5e40935a0039662cf0e330dcdc1bb3891feb2d13
Ritansh Rajput <it2042@global.org.in>
2023-06-24 02:39:01 +0530

    day119

--- zPractice.cpp
+++ zPractice.cpp
@@ -1265,3 +1265,137 @@
 
 // 🔸time complexity: O(n1 + n2 + n3)
 // 🔸 space complexity: O(n1 + n2 + n3)
+
+
+//               //❓Question: Alternate positive and negative numbers
+
+// Given an unsorted array Arr of N positive and negative numbers. Your task is to create an array of alternate positive and negative numbers without changing the relative order of positive and negative numbers.
+// Note: Array should start with a positive number and 0 (zero) should be considered a positive element.
+ 
+
+// Example 1:
+// Input: 
+// N = 9
+// Arr[] = {9, 4, -2, -1, 5, 0, -5, -3, 2}
+// Output:
+// 9 -2 4 -1 5 -5 0 -3 2
+// Explanation : Positive elements : 9,4,5,0,2
+// Negative elements : -2,-1,-5,-3
+// As we need to maintain the relative order of
+// postive elements and negative elements we will pick
+// each element from the positive and negative and will
+// store them. If any of the positive and negative numbers
+// are completed. we will continue with the remaining signed
+// elements.The output is 9,-2,4,-1,5,-5,0,-3,2.
+
+// Example 2:
+// Input:
+// N = 10
+// Arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8}
+// Output:
+// 5 -5 2 -2 4 -8 7 1 8 0
+// Explanation : Positive elements : 5,2,4,7,1,8,0
+// Negative elements : -5,-2,-8
+// As we need to maintain the relative order of
+// postive elements and negative elements we will pick
+// each element from the positive and negative and will
+// store them. If any of the positive and negative numbers
+// are completed. we will continue with the remaining signed
+// elements.The output is 5,-5,2,-2,4,-8,7,1,8,0.
+
+// Expected Time Complexity: O(N)
+// Expected Auxiliary Space: O(N)
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+	// void rearrange(int arr[], int n) 
+    // {
+      
+    //     vector<int> pos;
+    //     vector<int> neg;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(arr[i] <0) neg.push_back(arr[i]);
+    //         else pos.push_back(arr[i]);
+    //     };
+        
+    //     int i=0,j = 0,k=0;
+    //     while( i <n )
+    //     {
+
+    //        if(j != pos.size())
+    //         {
+    //             arr[i] = pos[j];
+    //             j++;
+    //             i++;
+    //         }
+    //         if( k!=neg.size())
+    //         {
+    //             arr[i] = neg[k];
+    //             k++;
+    //             i++;
+    //         }
+    //     }
+    // }
+// 🔸 Time Complexity: O(N)
+// 🔸 Space complexity : O(N)
+
+
+
+//                  //❓Question: Subarray with 0 Sum
+
+// Given an array of positive and negative numbers.
+// Find if there is a subarray (of size at-least one) with 0 sum.
+
+// Example 1:
+// Input:
+// 5
+// 4 2 -3 1 6
+
+// Output: 
+// Yes
+// Explanation: 
+// 2, -3, 1 is the subarray 
+// with sum 0.
+
+// Example 2:
+// Input:
+// 5
+// 4 2 0 1 6
+
+// Output: 
+// Yes
+// Explanation: 
+// 0 is one of the element 
+// in the array so there exist a 
+// subarray with sum 0.
+
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+//  bool subArrayExists(int arr[], int n)
+//     {
+//         unordered_map<int, bool> m;
+//         m[0] = true;
+        
+//         int sum = 0;
+        
+//         for(int i=0;i<n;i++){
+            
+//             sum += arr[i];
+            
+//             if(m[sum]) {
+//                 return true;
+//             }
+//             else {
+//                 m[sum] = true;
+//             }
+//         }
+        
+//         return false;
+//     }
+
+//🔸time complexity: O(n)
+// 🔸space complexity: O(n)
