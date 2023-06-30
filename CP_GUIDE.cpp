79481a4faed807a19c679371e6b69e498c1de4a9
RitanshRajput <it2042@global.org.in>
2023-07-01 01:13:09 +0530

    day123

---
+++ CP_GUIDE.cpp
@@ -0,0 +1,67 @@
+
+//                  //🔴🔴 COMPETITIVE PROGRAMMING GUIDE
+
+// 🔴01
+// 🔸CP Prerequisite
+// 🔸CP & Algortithms : Importance & Motivation of CP, Time and Space Complexities, Analysis of Common Techniques
+// 🔸DSA Part I : Arrays, Matrix, Recursion, Hashing, String, Searching & Sorting
+// 🔸DSA Part II : Stack, Queue & Deque, Tree, BST, Heap, Graph, Greedy & Backtracking
+// 🔸DSA Part III : Dynamic Programming, Trie, Segment & Binary Indexed Tree & Disjoint Set
+
+// 🔴02
+// 🔸CP Basics & Number Theory
+// 🔸Basics : Solving First Competitive Programming Problem, Ranges of input And Output, Tricks For CP, Floating Point Format, etc
+// 🔸Number Theory I : Bit Masking, Fibonnaci Numbers, Divisors, Prime Factorization, Prime Numbers
+// 🔸Number Theory II : GCD & LCM, Mathematical Principles, Number Theoretic Functions, Binomial Coefficients
+// 🔸Number Theory III : Catalan Number, Modular Arithmetic, Modular Exponentiation, Combinatorial Game Theory, Geometric Algorithms, Miscellaneous Problems
+
+
+// 🔴03
+// 🔸Recursion and Backtracking
+// 🔸Recursion : Generate all the balanced string of parentheses for given length, Generate all passwords of given length, Number of Paths in a Grid, Binomial Coefficient, Text Pattern Matching, etc
+// 🔸Backtracking : Sudoku Problem, N-Queen, Rat in a Maze, The Knight's tour, etc
+
+
+// 🔴04
+// 🔸Divide and Conquer
+// 🔸Binary Search : Binary Search in CP, Faster Implementation of BS and Concept of Invariants, Need for Monotonic function and practice problems
+// 🔸Ternay Search : Ternary Search concept, Need for Parabolic Curve, Counting Inversions using Merge Sort, Minimum Range Problem, Peak in a rotated array, etc
+
+
+// 🔴05
+// 🔸Stacks and Queues
+// 🔸Stack : Next Greater Element, Valid parenthesis, Maximum in a stack in O(1) time complexity and O(1) space, Largest Rectangle in a Histogram, Trapping Rain Water, etc
+// 🔸Queue : Maximum subsequence sum with some constraints, Maximum in a window, etc
+// 🔸Priority Queue : Minimize the Cost, Skyline Problem, Median of a Running Stream, etc
+
+
+// 🔴06
+// 🔸Trees
+// 🔸Traversals in Trees : Implementation of Inorder, Preorder and Postoder Traversal, Level Order Traversal, etc
+// 🔸Extra Concepts and Problem Solving : LCA of Binary Tree, Euler Tour Problem, etc
+
+// 🔴07
+// 🔸Greedy
+// 🔸Greedy vs DP, Policemen and Theives Problems, Fractional Knapsack Problem, Xor Problem, Job Sequencing Problem, Dijkstra on grid etc
+
+
+// 🔴08
+// 🔸Dynamic Programming
+// 🔸DP Basics : Standard DP Algorithms including Kadane's Algorithms, Matrix Chain Multiplication, 2D DP, Combinatorics and problem solving
+// 🔸GeekyBlitz - Contest I
+// 🔸DP Advanced : Range DP, Bitmask DP, Expected Value, Probability DP and Digit DP with concept and problems
+
+
+// 🔴09
+// 🔸Graphs
+// 🔸BFS, DFS, Topological Sort, Lowest Common Ancestor, Bridges and Articulation Point, Euler Path and Circuit, Bipartite Graph, Disjoint Sets, Strongly Connected Components, Single Shortest Path, Spanning Tree, etc
+
+
+// 🔴10
+// 🔸String
+// 🔸Naive Pattern Searching, Rabin Karp, KMP Algorithm, Z function, String Suffix Structure, etc
+
+
+// 🔴11
+// 🔸Range Query Problems
+// 🔸Sparse Table, Segment Tree, Merge Sort Tree, Lazy Propagation, Fenwick Tree, Range Query Problems and much more
