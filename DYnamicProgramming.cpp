71b2b20a309c0da22b49643b43a3300a6ef46399
RitanshRajput <it2042@global.org.in>
2023-07-01 22:59:34 +0530

    day124

--- DYnamicProgramming.cpp
+++ DYnamicProgramming.cpp
@@ -3428,4 +3428,197 @@
 //🔸time complexity: O(n^2)
 //🔸space complexity: O(n^2)
 
-//  130 / 149
+
+
+//               //❓Question: Pizzza with 3n slices (hard)
+
+// There is a pizza with 3n slices of varying size, you and your friends will take slices of pizza as follows:
+
+// You will pick any pizza slice.
+// Your friend Alice will pick the next slice in the anti-clockwise direction of your pick.
+// Your friend Bob will pick the next slice in the clockwise direction of your pick.
+// Repeat until there are no more slices of pizzas.
+
+// Given an integer array slices that represent the sizes of the pizza slices in a clockwise direction, 
+// return the maximum possible sum of slice sizes that you can pick.
+
+// example 1:
+//  Input: slices = [1,2,3,4,5,6]
+// Output: 10
+// Explanation: Pick pizza slice of size 4, Alice and Bob will pick slices with size 3 and 5 respectively. 
+// Then Pick slices with size 6, finally Alice and Bob will pick slice of size 2 and 1 respectively. 
+// Total = 4 + 6.
+
+//example 2:
+// Input: slices = [8,9,8,6,1,1]
+// Output: 16
+// Explanation: Pick pizza slice of size 8 in each turn. 
+// If you pick slice with size 9 your partners will pick slices of size 8.
+ 
+
+// Constraints:
+// 3 * n == slices.length
+// 1 <= slices.length <= 500
+// 1 <= slices[i] <= 1000
+
+
+// //🔴appraoch: Recursion 
+// //🔸time limit exceeded
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int solve(int index, int endIndex, vector<int> &slices, int n) {     // n = total no. of slices we will eat
+//     //base case
+//     if( n==0 || index > endIndex) { 
+//         return 0;
+//     }
+
+//     int eat = slices[index] + solve(index+2, endIndex,  slices, n-1) ;
+//     int notEat = 0 + solve(index+1, endIndex, slices, n ) ;
+//     return max(eat, notEat) ;
+// }
+
+// int maxSizeSlices(vector<int> &slices) {
+//     int k = slices.size() ;
+//     int case1 = solve(0, k-2, slices, k/3) ;    // eating from 0th slice
+//     int case2 = solve(1, k-1 ,slices, k/3) ;    // eating from 1th slice
+//     return max(case1, case2) ;
+// }
+// //🔸time complexity: expo
+// //🔸space complexity: expo
+
+
+//🔴appraoch: Recursion + Memoization (top-down approach)
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int solve(int index, int endIndex, vector<int> &slices, int n, vector<vector<int>> &dp) {     
+//     //base case
+//     if( n==0 || index > endIndex) { 
+//         return 0;
+//     }
+
+//     if(dp[index][n] != -1){
+//         return dp[index][n] ;
+//     }
+
+//     int eat = slices[index] + solve(index+2, endIndex,  slices, n-1, dp) ;
+//     int notEat = 0 + solve(index+1, endIndex, slices, n , dp) ;
+//     return  dp[index][n] = max(eat, notEat) ;
+// }
+
+// int maxSizeSlices(vector<int> &slices) {
+//     int k = slices.size() ;
+//     //two different scenarios will require 2 different dp array
+//     vector<vector<int>> dp1(k, vector<int>(k, -1)) ;  
+//     int case1 = solve(0, k-2, slices, k/3, dp1) ;   
+
+//     vector<vector<int>> dp2(k, vector<int>(k, -1)) ;
+//     int case2 = solve(1, k-1 ,slices, k/3, dp2) ;   
+//     return max(case1, case2) ;
+// }
+//🔸time complexity:  O(K^2)
+//🔸space complexity: O(K^2)
+
+
+//🔴appraoch: Tabulation (down-top approach)
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int solve(vector<int> &slices) {     
+
+//     int k = slices.size() ;
+
+//     vector<vector<int>> dp1(k+2, vector<int>(k+2, 0)) ;  
+//     vector<vector<int>> dp2(k+2, vector<int>(k+2, 0)) ;  
+
+//     for(int index = k-2;  index >= 0; index--) {
+//         for(int n = 1; n <= k/3; n++) {
+//             int eat = slices[index] +  dp1[index+2][n-1] ;
+//             int notEat = 0 + dp1[index+1][n] ;
+
+//             dp1[index][n] = max(eat, notEat) ;
+//         }
+//     }
+
+//     int case1 = dp1[0][k/3] ;
+
+//     for(int index = k-1;  index >= 1; index--) {
+//         for(int n = 1; n <= k/3; n++) {
+//             int eat = slices[index] +  dp2[index+2][n-1] ;
+//             int notEat = 0 + dp2[index+1][n] ;
+
+//             dp2[index][n] = max(eat, notEat) ;
+//         }
+//     }
+
+//     int case2 = dp2[1][k/3] ;
+
+//     return max(case1, case2) ;
+// }
+
+// int maxSizeSlices(vector<int> &slices) {
+//     return solve(slices) ;
+// }
+//🔸time complexity: O(K^2)
+//🔸space complexity: O(k^2)         // slices of array size
+
+
+
+//🔴appraoch: space Optimisation
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int solve(vector<int> &slices) {     
+
+//     int k = slices.size() ;
+
+//     vector<int> prev1(k+2, 0) ;
+//     vector<int> curr1(k+2, 0) ;
+//     vector<int> next1(k+2, 0) ;
+
+//     vector<int> prev2(k+2, 0) ;
+//     vector<int> curr2(k+2, 0) ;
+//     vector<int> next2(k+2, 0) ;
+
+//     for(int index = k-2;  index >= 0; index--) {
+//         for(int n = 1; n <= k/3; n++) {
+//             int eat = slices[index] +  next1[n-1] ;
+//             int notEat = 0 + curr1[n] ;
+
+//             prev1[n] = max(eat, notEat) ;
+//         }
+//         next1 = curr1 ;
+//         curr1 = prev1 ;
+//     }
+
+//     int case1 = curr1[k/3] ;
+
+//     for(int index = k-1;  index >= 1; index--) {
+//         for(int n = 1; n <= k/3; n++) {
+//             int eat = slices[index] +  next2[n-1] ;
+//             int notEat = 0 + curr2[n] ;
+
+//             prev2[n] = max(eat, notEat) ;
+//         }
+//         next2 = curr2 ;
+//         curr2 = prev2 ;
+//     }
+
+//     int case2 = curr2[k/3] ;
+
+//     return max(case1, case2) ;
+// }
+
+// int maxSizeSlices(vector<int> &slices) {
+//     return solve(slices) ;
+// }
+//🔸time complexity:  O(K^2)
+//🔸space complexity: O(6*k)
+
+
+// 131 / 149
