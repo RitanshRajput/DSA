c966f7c23f1bf29b303eb2173a55c5a648f893dc
RitanshRajput <it2042@global.org.in>
2023-07-13 01:01:40 +0530

    day131

--- zPractice.cpp
+++ zPractice.cpp
@@ -2206,3 +2206,74 @@
    
 //      return ans;  
 //     }
+
+
+//                   //❓Question: Unique Number of Occurences
+
+// Given an array arr of N integers, 
+// the task is to check whether the frequency of the elements in the array is unique or not. 
+// Or in other words, there are no two distinct numbers in array with equal frequency. 
+// If all the frequency is unique then return true, else return false.
+
+// Example 1:
+// Input:
+// N = 5
+// arr = [1, 1, 2, 5, 5]
+// Output:
+// false
+// Explanation:
+// The array contains 2 (1’s), 1 (2’s) and 2 (5’s), since the number of frequency of 1 and 5 are the same i.e. 2 times. 
+// Therefore, this array does not satisfy the condition.
+// Example 2:
+
+// Input:
+// N = 10
+// arr = [2, 2, 5, 10, 1, 2, 10, 5, 10, 2]
+// Output:
+// true
+// Explanation:
+// Number of 1’s -> 1
+// Number of 2’s -> 4
+// Number of 5’s -> 2
+// Number of 10’s -> 3.
+// Since, the number of occurrences of elements present in the array is unique. 
+// Therefore, this array satisfy the condition.
+// Your task:
+// You don't need to read input or print anything. 
+// Your task is to complete the function isFrequencyUnique() which take integer N and array arr of size N as arguments, 
+// and returns a boolean.
+
+// Constraints:
+// 1 <= N <=105
+// -109 <= arr[i] <= 109
+
+//🔴approach: 
+
+//  bool isFrequencyUnique(int n, int arr[])
+//     {
+//         map<int, int> mp; 
+        
+//         for(int i=0; i<n; i++) {
+//             mp[arr[i]]++ ;
+//         }
+        
+//         vector<int> v;
+//         for(auto i:mp) {
+//             v.push_back(i.second) ;
+//         }
+        
+//         set<int> ans ;
+//         for(int i=0; i<v.size(); i++) {
+//             ans.insert(v[i]) ;
+//         }
+        
+//         if(ans.size() == v.size()) {
+//             return true ;
+//         }
+//         else{
+//             return false ;
+//         }
+        
+//     }
+//🔸time complexity: O(N)
+//🔸space complexity: O(N)
