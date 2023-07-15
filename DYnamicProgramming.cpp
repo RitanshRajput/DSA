ecae0bd45cd2c327484d25eb61d10adc7ff3cac7
RitanshRajput <it2042@global.org.in>
2023-07-16 01:33:24 +0530

    day135

--- DYnamicProgramming.cpp
+++ DYnamicProgramming.cpp
@@ -4430,4 +4430,64 @@
 //🔸 time complexity : O(n^2)
 //🔸 space complexity : O(N^2)
 
-// 135 / 149
+
+//               //❓Question: Longest Arithmetic Subsequence of Given Difference
+
+// Given an integer array arr and an integer difference, 
+// return the length of the longest subsequence in arr which is an arithmetic sequence 
+// such that the difference between adjacent elements in the subsequence equals difference.
+
+// A subsequence is a sequence that can be derived from arr by deleting some or 
+// no elements without changing the order of the remaining elements.
+
+
+// Example 1:
+// Input: arr = [1,2,3,4], difference = 1
+// Output: 4
+// Explanation: The longest arithmetic subsequence is [1,2,3,4].
+
+// Example 2:
+// Input: arr = [1,3,5,7], difference = 1
+// Output: 1
+// Explanation: The longest arithmetic subsequence is any single element.
+
+// Example 3:
+// Input: arr = [1,5,7,8,5,3,4,2,1], difference = -2
+// Output: 4
+// Explanation: The longest arithmetic subsequence is [7,5,3,1].
+
+// Constraints:
+
+// 1 <= arr.length <= 105
+// -104 <= arr[i], difference <= 104
+
+//🔴Approach : Recursion
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+int longestSubsequence(vector<int>& arr, int difference) {
+
+    unordered_map<int, int> dp ;
+    int ans = 0 ;
+
+    for(int i=0; i<arr.size(); i++) {
+        int temp = arr[i] - difference ;
+        int tempAns = 0 ;
+
+        //check ans exist for temp already or not
+        if(dp.count(temp)) {        // check dp me temp ka answer hai ya nahi
+            tempAns = dp[temp] ;
+        }
+
+        //update current answer
+        dp[arr[i]] = 1 + tempAns ;
+
+        // update main ans 
+        ans = max(ans, dp[arr[i]]) ;
+    }
+        return ans ;
+}
+//🔸time complexity : O(N)
+//🔸space complexity : O(n)
