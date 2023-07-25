3301db2a4beeaa3ac0da51719f8fe0752772d946
RitanshRajput <it2042@global.org.in>
2023-07-26 00:58:45 +0530

    day145

--- DYnamicProgramming.cpp
+++ DYnamicProgramming.cpp
@@ -5848,4 +5848,307 @@
 //🔸space complexity: O(n)
 
 
-// 145 / 149
+
+//                 //❓Question: 516. Longest Palindromic SUbsequence
+
+// Given a string s, find the longest palindromic subsequence's length in s.
+// A subsequence is a sequence that can be derived from another sequence by deleting some
+// or no elements without changing the order of the remaining elements.
+
+// Example 1:
+// Input: s = "bbbab"
+// Output: 4
+// Explanation: One possible longest palindromic subsequence is "bbbb".
+
+// Example 2:
+// Input: s = "cbbd"
+// Output: 2
+// Explanation: One possible longest palindromic subsequence is "bb".
+ 
+// Constraints:
+// 1 <= s.length <= 1000
+// s consists only of lowercase English letters.
+
+//🔴approach : using previous questions optimised approach 
+// s = "cbbd" 
+// revStr = "dbbc"  //reverse original string
+// longest subsequence from both the string will be answer
+
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
+
+// int solve(string& a, string& b) {
+
+//   vector<int> curr(a.length()+1, 0) ;
+//   vector<int> next(b.length()+1, 0) ;
+  
+//   for(int i=a.length()-1; i>=0; i--){
+//     for(int j=b.length()-1; j>=0; j--) {
+        
+//       int ans = 0 ;
+
+//       if( a[i] == b[j]) {
+//         ans = 1 + next[j+1] ;
+//       }
+//       else{
+//         ans = max(next[j], curr[j+1]) ;
+//       }
+      
+//       curr[j] = ans ;
+//     }
+//     next = curr ;
+//   }
+//     return next[0] ;
+// }
+
+//  int longestPalindromeSubseq(string s) {
+//         string reverseStr = s;
+//         reverse(reverseStr.begin(), reverseStr.end()) ;
+//         int ans = solve(s, reverseStr) ;
+//         return  ans ;
+// }
+//🔸time complexity:  O(N*N)
+//🔸space complexity: O(n)
+
+
+
+//                     //❓Question: 72. Edit Distance
+
+// Given two strings word1 and word2, 
+// return the minimum number of operations required to convert word1 to word2.
+
+// You have the following three operations permitted on a word:
+
+// Insert a character
+// Delete a character
+// Replace a character
+ 
+// Example 1:
+// Input: word1 = "horse", word2 = "ros"
+// Output: 3
+// Explanation: 
+// horse -> rorse (replace 'h' with 'r')
+// rorse -> rose (remove 'r')
+// rose -> ros (remove 'e')
+
+// Example 2:
+// Input: word1 = "intention", word2 = "execution"
+// Output: 5
+// Explanation: 
+// intention -> inention (remove 't')
+// inention -> enention (replace 'i' with 'e')
+// enention -> exention (replace 'n' with 'x')
+// exention -> exection (replace 'n' with 'c')
+// exection -> execution (insert 'u')
+ 
+// Constraints:
+// 0 <= word1.length, word2.length <= 500
+// word1 and word2 consist of lowercase English letters.
+
+// //🔴approch : Recursion
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int solve(string& a, string& b, int i, int j) {
+//     //base case
+//     if( i == a.length()) {
+//         return b.length() - j;
+//     }
+
+//     if( j == b.length()) {
+//         return a.length() - i;
+//     }
+
+//     int ans = 0;
+//     if(a[i] == b[j]) {
+//         return solve(a, b, i+1, j+1) ;
+//     }
+//     else{
+//         //insert 
+//         int insertAns = 1 + solve(a, b, i, j+1);
+
+//         //delete
+//         int deleteAns = 1 + solve(a, b, i+1, j) ;
+
+//         //replcae
+//         int replaceAns = 1 + solve(a, b, i+1, j+1) ;
+
+//         ans = min(insertAns, min(deleteAns, replaceAns)) ;
+//     }
+
+//     return ans ;
+// }
+
+//  int minDistance(string word1, string word2) {
+//         return solve(word1, word2, 0, 0) ;
+// }
+// //🔸time complexity: expo
+// //🔸space complexity: O(n)         // length of string
+
+
+// //🔴approch : Recursion + Memoization
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int solve(string& a, string& b, int i, int j, vector<vector<int>>& dp) {
+//     //base case
+//     if( i == a.length()) {
+//         return b.length() - j;
+//     }
+
+//     if( j == b.length()) {
+//         return a.length() - i;
+//     }
+
+//     if(dp[i][j] != -1){
+//         return dp[i][j] ;
+//     }
+
+//     int ans = 0;
+//     if(a[i] == b[j]) {
+//         return solve(a, b, i+1, j+1, dp) ;
+//     }
+//     else{
+//         //insert 
+//         int insertAns = 1 + solve(a, b, i, j+1, dp);
+
+//         //delete
+//         int deleteAns = 1 + solve(a, b, i+1, j, dp) ;
+
+//         //replcae
+//         int replaceAns = 1 + solve(a, b, i+1, j+1, dp) ;
+
+//         ans = min(insertAns, min(deleteAns, replaceAns)) ;
+//     }
+
+//     return dp[i][j] = ans ;
+// }
+
+//  int minDistance(string word1, string word2) {
+
+//     vector<vector<int>> dp(word1.length(), vector<int>(word2.length(), -1));
+//     return solve(word1, word2, 0, 0, dp) ;
+// }
+// //🔸time complexity: O(n*m)
+// //🔸space complexity: O(n)         // length of minimum string
+
+
+
+// //🔴approch : Tabulation
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int solve(string& a, string& b) {
+
+//     vector<vector<int>> dp(a.length()+1, vector<int>(b.length()+1, 0));
+
+//     //base case
+//     for(int j=0; j<b.length(); j++){
+//         dp[a.length()][j] = b.length() - j ;
+//     }
+
+//     for(int i=0; i<a.length(); i++){
+//         dp[i][b.length()] = a.length() - i ;
+//     }
+
+
+//     for( int i=a.length()-1; i>=0; i--){
+//         for(int j=b.length()-1; j>=0; j--){
+
+//             int ans = 0;
+
+//             if(a[i] == b[j]) {
+//                 ans = dp[i+1][j+1];
+//             }
+//             else{
+//                 //insert 
+//                 int insertAns = 1 + dp[i][j+1];
+
+//                 //delete
+//                 int deleteAns = 1 + dp[i+1][j] ;
+
+//                 //replcae
+//                 int replaceAns = 1 + dp[i+1][j+1] ;
+
+//                 ans = min(insertAns, min(deleteAns, replaceAns)) ;
+//             }
+
+//             dp[i][j] = ans ;
+//         }
+//     }
+
+//     return dp[0][0] ;
+// }
+
+//  int minDistance(string word1, string word2) {
+
+//     return solve(word1, word2) ;
+// }
+// //🔸time complexity: O(n*m)
+// //🔸space complexity: O(n)         // length of minimum string
+
+
+//🔴approch : space optimisation
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int solve(string& a, string& b) {
+
+//     vector<int> curr(b.length()+1, 0) ;
+//     vector<int> next(b.length()+1, 0) ;
+
+//     //base case
+//     for(int j=0; j<b.length(); j++){
+//        next[j] = b.length() - j ;
+//     }
+
+//     for( int i=a.length()-1; i>=0; i--){
+//         for(int j=b.length()-1; j>=0; j--){
+
+//             //important line // got from 2nd base case
+//             curr[b.length()] = a.length() - i;
+
+//             int ans = 0;
+
+//             if(a[i] == b[j]) {
+//                 ans = next[j+1];
+//             }
+//             else{
+//                 //insert 
+//                 int insertAns = 1 + curr[j+1];
+
+//                 //delete
+//                 int deleteAns = 1 + next[j] ;
+
+//                 //replcae
+//                 int replaceAns = 1 + next[j+1] ;
+
+//                 ans = min(insertAns, min(deleteAns, replaceAns)) ;
+//             }
+//             curr[j] = ans ;
+//         }
+//         next = curr ;
+//     }
+
+//     return next[0] ;
+// }
+
+//  int minDistance(string word1, string word2) {
+
+//     if(word1.length() == 0){
+//         return word2.length();
+//     }
+//     if(word2.length() == 0){
+//         return word1.length() ;
+//     }
+
+//     return solve(word1, word2) ;
+// }
+//🔸time complexity: O(n*m)
+//🔸space complexity: O(n)         // length of minimum string
