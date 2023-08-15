961fb1a771f1936831a85a7e8ae6ffc04bd37339
RitanshRajput <it2042@global.org.in>
2023-08-15 10:22:59 +0530

    Day154

--- zPractice.cpp
+++ zPractice.cpp
@@ -4978,3 +4978,57 @@
 
 //🔸time complexity: O(N^2)
 //🔸space complexity: O(B.size() + A.length())
+
+
+
+
+//                   ///❓Question: Search Pattern (Rabin-Karp Algorithm)
+
+// Given two strings, one is a text string and other is a pattern string. 
+// The task is to print the indexes of all the occurences of pattern string in the text string.
+//  For printing, Starting Index of a string should be taken as 1.
+
+// Example 1:
+// Input:
+// S = "batmanandrobinarebat", pat = "bat"
+// Output: 1 18
+// Explanation: The string "bat" occurs twice
+// in S, one starts are index 1 and the other
+// at index 18. 
+// â€‹Example 2:
+
+// Input: 
+// S = "abesdu", pat = "edu"
+// Output: -1
+// Explanation: There's not substring "edu"
+// present in S.
+
+// Your Task:
+// You don't need to read input or print anything. Your task is to complete the function search() which takes the string S and the string pat as inputs and returns an array denoting the start indices (1-based) of substring pat in the string S. 
+
+// Expected Time Complexity: O(|S|*|pat|).
+// Expected Auxiliary Space: O(1).
+
+// Constraints:
+// 1<=|S|<=105
+// 1<=|pat|<|S|
+
+//🔸approach : 
+//  vector <int> search(string pat, string txt)
+//         {
+//            vector<int> result;
+        
+//         int n = txt.size()-pat.size();
+//         for(int i=0; i<=n; i++)
+//         {
+//             string temp = txt.substr(i, pat.size());
+//             if(pat==temp)
+//                 result.push_back(i+1);
+//         }
+//         if(result.size()==0)
+//             result.push_back(-1);
+//         return result;
+
+//         }
+//🔴time  complexity: O(n)         // n = txt.size()-pat.size();
+//🔴space complexity: O(n)         // number of text found in pat
