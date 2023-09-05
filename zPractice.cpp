3324df8feb2d125a9fd101c9335339987e91a62c
RitanshRajput <it2042@global.org.in>
2023-09-06 00:35:29 +0530

    Day166

--- zPractice.cpp
+++ zPractice.cpp
@@ -7851,3 +7851,62 @@
 //   }
 //🔸time complexity: O(N)
 //🔸space complexity: O(N+1)
+
+
+//               //❓Question: Count triplets with sum smaller than sum
+
+// Given an array arr[] of distinct integers of size N and a value sum, the task is to find the count of triplets (i, j, k), having (i<j<k) with the sum of (arr[i] + arr[j] + arr[k]) smaller than the given value sum.
+
+// Example 1:
+// Input: N = 4, sum = 2
+// arr[] = {-2, 0, 1, 3}
+// Output:  2
+// Explanation: Below are triplets with 
+// sum less than 2 (-2, 0, 1) and (-2, 0, 3). 
+ 
+// Example 2:
+// Input: N = 5, sum = 12
+// arr[] = {5, 1, 3, 4, 7}
+// Output: 4
+// Explanation: Below are triplets with 
+// sum less than 12 (1, 3, 4), (1, 3, 5), 
+// (1, 3, 7) and (1, 4, 5).
+
+// Your Task:
+// This is a function problem. You don't need to take any input, as it is already accomplished by the driver code. You just need to complete the function countTriplets() that take array arr[], integer N  and integer sum as parameters and returns the count of triplets.
+
+// Expected Time Complexity: O(N2).
+// Expected Auxiliary Space: O(1).
+
+// Constraints:
+// 3 ≤ N ≤ 103
+// -103 ≤ arr[i] ≤ 103
+
+//🔴approach :
+// long long countTriplets(long long arr[], int n, long long sum)
+// 	{
+// 	    sort(arr,arr+n);
+//         long long count  = 0;
+//         // will go till third last element
+//         for(int i = 0;i<n-2;i++)  {
+//             // always remains 1 step ahead to i
+//             int left = i+1;
+//             // remains at end
+//             int right = n-1;
+           
+//             while(left < right) {
+//                 if(arr[i]+ arr[left] + arr[right] < sum) {
+//                     // if above condition is satisfied then the number of possible triplets
+//                     // in the range i to right will be? => right - left
+//                     count+=(right-left);
+//                     left++;
+//                 }
+//                 else
+//                 right--;
+//             }
+//         }  
+//         return count;
+// 	}
+//🔸time complexity: O(N^2)
+//🔸space complexity: O(1)
+
