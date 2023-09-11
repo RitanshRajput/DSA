4e7a6553fb19275110b0ce88a49eead2a03f6c5f
RitanshRajput <it2042@global.org.in>
2023-09-11 23:59:23 +0530

    day171

--- zPractice.cpp
+++ zPractice.cpp
@@ -8555,3 +8555,92 @@
     
 //     return 0;
 // }
+
+
+//             /❓Question: k-th element of two arrays
+
+// Given two arrays arr1 and arr2 of size N and M respectively and an element K. 
+// The task is to find the element that would be at the kth position of the final sorted array.
+ 
+// Example 1:
+// Input:
+// arr1[] = {2, 3, 6, 7, 9}
+// arr2[] = {1, 4, 8, 10}
+// k = 5
+// Output:
+// 6
+// Explanation:
+// The final sorted array would be -
+// 1, 2, 3, 4, 6, 7, 8, 9, 10
+// The 5th element of this array is 6.
+
+// Example 2:
+// Input:
+// arr1[] = {100, 112, 256, 349, 770}
+// arr2[] = {72, 86, 113, 119, 265, 445, 892}
+// k = 7
+// Output:
+// 256
+// Explanation:
+// Final sorted array is - 72, 86, 100, 112,
+// 113, 119, 256, 265, 349, 445, 770, 892
+// 7th element of this array is 256.
+
+// Your Task:  
+// You don't need to read input or print anything. Your task is to complete the function kthElement() which takes the arrays arr1[], arr2[], its size N and M respectively and an integer K as inputs and returns the element at the Kth position.
+
+// Expected Time Complexity: O(Log(N) + Log(M))
+// Expected Auxiliary Space: O(Log (N))
+
+// Constraints:
+// 1 <= N, M <= 106
+// 0 <= arr1i, arr2i < INT_MAX
+// 1 <= K <= N+M
+
+//🔴approach:
+// int kthElement(int arr1[], int arr2[], int n, int m, int k)
+//     {
+//         vector<int> ans;
+//         for(int i=0; i<n; i++){
+//             ans.push_back(arr1[i]);
+//         }
+//         for(int i=0; i<m; i++){
+//             ans.push_back(arr2[i]);
+//         }
+        
+//         sort(ans.begin(), ans.end()) ;
+        
+//         return ans[k-1];
+//     }
+//🔸time complexity : O(n+m * logn)
+//🔸space complexity : O(n+m)
+
+
+//🔴approach : optimised
+// int kthElement(int arr1[], int arr2[], int n, int m, int k)
+//     {
+//         int left = n-1;
+//         int right = 0;
+            
+//         while( right < m && left >= 0 ) {
+//             if(arr1[left] >= arr2[right]) {
+//                 swap(arr1[left], arr2[right]) ;
+//                 left--;
+//                 right++ ;
+//             }
+//             else break;
+//         }
+//         sort(arr1, arr1+n) ;
+//         sort(arr2, arr2+m) ; 
+        
+//         if(k<=n){
+//             return arr1[k-1];
+//         }
+//         else{
+//            return arr2[k-n-1];
+//         }
+//     }
+//🔸time complexity : O(N+M logn)
+//🔸space complexity: O(1)
+
+
