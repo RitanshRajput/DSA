51a4480e38555f6b3df99a9326e52b1f0f125de3
RitanshRajput <it2042@global.org.in>
2023-09-20 21:54:26 +0530

    day176

--- zPractice.cpp
+++ zPractice.cpp
@@ -9463,3 +9463,67 @@
 // }
 
 
+
+
+//                //❓Question: Minimum operations to reduce X to zero 
+
+// You are given an integer array nums and an integer x. In one operation, 
+// you can either remove the leftmost or the rightmost element from the array nums and subtract its value from x.
+// Note that this modifies the array for future operations.
+// Return the minimum number of operations to reduce x to exactly 0 if it is possible, otherwise, return -1.
+
+// Example 1:
+// Input: nums = [1,1,4,2,3], x = 5
+// Output: 2
+// Explanation: The optimal solution is to remove the last two elements to reduce x to zero.
+
+// Example 2:
+// Input: nums = [5,6,7,8,9], x = 4
+// Output: -1
+
+// Example 3:
+// Input: nums = [3,2,20,1,1,3], x = 10
+// Output: 5
+// Explanation: The optimal solution is to remove the last three elements and the first two elements (5 operations in total) to reduce x to zero.
+ 
+// Constraints:
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 104
+// 1 <= x <= 109
+
+//🔴appraoch : Two POinter and sliding window
+// int minOperations(vector<int>& nums, int x) {
+        
+//         int total = accumulate(nums.begin(), nums.end(), 0) ;
+//         int target = total - x ;
+        
+//         if(target < 0 ) return -1;
+//         if(target == 0) return nums.size() ;
+        
+//         int minSum = INT_MAX ;
+//         int currSum = 0 ;
+//         int left = 0 ;
+//         int right = 0 ;
+//         int n = nums.size() ;
+        
+//         while(right < n)  {
+//             currSum += nums[right] ;
+//             right++ ;
+            
+//             while(currSum > target && left < n) {
+//                 currSum -= nums[left] ;
+//                 left++ ;
+//             }
+            
+//             if(currSum == target) {
+//                 minSum = min(minSum, n-(right-left)) ;
+//             }
+//         }
+        
+//         return (minSum == INT_MAX) ? -1 : minSum ;
+        
+//     }
+
+//🔸time complexity: O(N^2)
+//🔸space complexity: O(N)
+
