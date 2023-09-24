0c6c8cd8eb27283964e445010bd48c1fcf704915
RitanshRajput <it2042@global.org.in>
2023-09-24 15:47:37 +0530

    day177

--- DSA_Sheet_solving.cpp/Sorting_and_Searching.cpp
+++ DSA_Sheet_solving.cpp/Sorting_and_Searching.cpp
@@ -2555,3 +2555,109 @@
 //🔸time complexity: O(N^2)
 //🔸space complexity: O(N)
 
+
+
+//                          //❓ Question: Roti-Paratha
+
+// IEEE is having its AGM next week and the president wants to serve cheese prata after the meeting.
+// The subcommittee members are asked to go to food connection and get P (P<=1000) pratas packed for the function.
+// The stall has L cooks (L<=50) and each cook has a rank R (1<=R<=8).
+// A cook with a rank R can cook 1 prata in the first R minutes 1 more prata in the next 2R minutes,
+// 1 more prata in 3R minutes and so on (he can only cook a complete prata) (For example if a cook is ranked 2,
+//  he will cook one prata in 2 minutes one more prata in the next 4 mins an one more in the next 
+// 6 minutes hence in total 12 minutes he cooks 3 pratas in 13 minutes also he can cook only 3 pratas 
+// as he does not have enough time for the 4th prata). The webmaster wants to know the minimum time to get the order done. 
+// Please write a program to help him out.
+
+// Input
+// The first line tells the number of test cases. Each test case consist of 2 lines. 
+// In the first line of the test case we have P the number of prata ordered. 
+// In the next line the first integer denotes the number of cooks L and L integers follow in the same line each denoting the rank of a cook.
+
+// Output
+// Print an integer which tells the number of minutes needed to get the order done.
+
+// Example
+// Input:
+// 3
+// 10
+// 4 1 2 3 4
+// 8
+// 1 1
+// 8
+// 8 1 1 1 1 1 1 1 1
+
+// Output:
+// 12
+// 36
+// 1
+
+//🔴approach : Binary search 
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// bool valid(long long t, long long rank[], long long int P, int n)
+// {
+// 	long long cp = 0, tt, val, count = 0;
+	
+// 	for(int i = 0; i < n; i++)
+// 	{
+// 		tt = rank[i];
+// 		count = 0;
+// 		val = 1;
+		
+// 		while(tt <= t)
+// 		{
+// 			count++;
+// 			val++;
+// 			tt += (val*rank[i]);
+// 		}
+		
+// 		cp += count;
+		
+// 		if(cp >= P) return true;
+// 	}
+// 	return false;
+// }
+
+// int main() 
+// {
+// 	long long t, P, n;
+// 	cin >> t;
+// 	while(t--)
+// 	{
+// 		cin >> P >> n;
+	
+// 		long long rank[n];
+	
+// 		for(int i = 0; i < n; i++)
+// 			cin >> rank[i];
+		
+// 		long long low = 1, high = INT_MIN;
+	
+// 		for(int i = 0; i < n; i++)
+// 			high = max(high, rank[i]*(P*(P+1)/2));
+	
+// 		long long ans = 0;
+		
+// 		while(low <= high)
+// 		{
+// 			long long mid = low + (high -low)/2;
+			
+// 			if(valid(mid, rank, P, n))
+// 			{
+// 				ans = mid;
+// 				high = mid-1;
+// 			}
+// 			else 
+// 				low = mid + 1;
+// 		}
+	
+// 		cout << ans << endl;
+// 	}
+	
+// 	return 0;
+// }
+//🔸time complexity: O(N* logN)
+//🔸space complexity: O(n)
