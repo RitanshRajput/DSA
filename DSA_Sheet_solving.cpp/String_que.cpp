495a99445b35467de200f3d0a704c81a2eeecd58
RitanshRajput <it2042@global.org.in>
2023-09-27 12:06:53 +0530

    day178

--- DSA_Sheet_solving.cpp/String_que.cpp
+++ DSA_Sheet_solving.cpp/String_que.cpp
@@ -3385,3 +3385,113 @@
 //🔸time complexity: O(n^m)
 //🔸space complexity: O(m)
 
+
+
+
+//                   //❓Question:  Decoded String at Index
+
+// You are given an encoded string s. To decode the string to a tape, 
+// the encoded string is read one character at a time and the following steps are taken:
+
+// If the character read is a letter, that letter is written onto the tape.
+// If the character read is a digit d, the entire current tape is repeatedly written d - 1 more times in total.
+// Given an integer k, return the kth letter (1-indexed) in the decoded string.
+
+// Example 1:
+// Input: s = "leet2code3", k = 10
+// Output: "o"
+// Explanation: The decoded string is "leetleetcodeleetleetcodeleetleetcode".
+// The 10th letter in the string is "o".
+
+// Example 2:
+// Input: s = "ha22", k = 5
+// Output: "h"
+// Explanation: The decoded string is "hahahaha".
+// The 5th letter is "h".
+// Example 3:
+
+// Input: s = "a2345678999999999999999", k = 1
+// Output: "a"
+// Explanation: The decoded string is "a" repeated 8301530446056247680 times.
+// The 1st letter is "a".
+ 
+
+// Constraints:
+// 2 <= s.length <= 100
+// s consists of lowercase English letters and digits 2 through 9.
+// s starts with a letter.
+// 1 <= k <= 109
+// It is guaranteed that k is less than or equal to the length of the decoded string.
+// The decoded string is guaranteed to have less than 263 letters.
+
+
+//🔴 approach : brute force appraoch : TLE
+//     string decodeAtIndex(string s, int k) {
+         
+//         string ans = "" ;
+//         int index =  1;
+//         int temp ;
+        
+//         for(int i=0; i<s.length(); i++) {
+            
+//             if(isdigit(s[i])) 
+//             {
+//                 string tempResult = ans ;
+//                 temp = int(s[i])-48 ;        //convert string digit into integer digit 
+//                 index = 1;
+//                 while(index < temp) 
+//                 {
+//                     for(int j=0; j<tempResult.length(); j++) 
+//                     { 
+//                        ans.push_back(tempResult[j]) ;  
+//                     }
+//                  index++ ;
+//                 }
+//              }
+//             else
+//             {
+//                 ans.push_back(s[i]) ;
+//             }
+//         }
+        
+//         string a ;
+//         a.push_back(ans[k-1]) ;
+//         return a ;
+//     }
+//🔸time complexity : O(N^2)
+//🔸space complexity: O(n)
+    
+
+//🔴optimised approach : using reverse and modulo :
+    
+//   string decodeAtIndex(string inputString, int k) {
+      
+//     long long decodedLength = 0;            // Total length of the decoded string
+      
+//     for (auto character : inputString) {
+//         if (isdigit(character)) {
+//                                             // If the character is a digit, update the decoded length accordingly
+//             decodedLength *= character - '0';
+//         } else {
+//                                              // If the character is a letter, increment the decoded length
+//             decodedLength++;
+//         }
+//     }
+//     // Traverse the input string in reverse to decode and find the kth character
+//     for (int i = inputString.size() - 1; i >= 0; i--) {
+//         if (isdigit(inputString[i])) {
+//                                                     // If the character is a digit, adjust the length and k accordingly
+//             decodedLength /= (inputString[i] - '0');
+//             k = k % decodedLength;
+//         } else {
+//                                                     // If the character is a letter, check if it's the kth character
+//             if (k == 0 || decodedLength == k)
+//                 return string("") + inputString[i]; // Return the kth character as a string
+            
+//             decodedLength--;
+//         }
+//     }
+//     return "";                                        // Return an empty string if no character is found
+// }
+//🔸time complexity : O(2N)
+//🔸space complexity: O(n)
