7e980c2a3418f3ac5f65fb3d810eb7f2bd1df62d
RitanshRajput <it2042@global.org.in>
2023-10-01 21:14:27 +0530

    day179

--- DSA_Sheet_solving.cpp/Matrix_que.cpp
+++ DSA_Sheet_solving.cpp/Matrix_que.cpp
@@ -933,3 +933,51 @@
 // grid[i][j] == 0 or 1
 
 //🔴Approach : 
+//  int numberOfEnclaves(vector<vector<int>> &grid) {
+        
+//         queue<pair<int,int>> q ;
+//         int n = grid.size() ;
+//         int m = grid[0].size() ;
+//         int vis[n][m] = {0} ;
+        
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<m; j++){
+//                 if(i==0 || j==0 || i == n-1 || j == m-1) {
+//                     if(grid[i][j] == 1){
+//                         q.push({i,j});
+//                         vis[i][j] = 1;
+//                     }
+//                 }
+//             }
+//         }
+        
+//         int delrow[4] = {-1, 0, 1, 0 } ;
+//         int delcol[4] = { 0, 1, 0, -1} ;
+        
+//         while( !q.empty()) {
+//             int row = q.front().first ;
+//             int col = q.front().second; 
+//             q.pop() ;
+            
+//             for(int i=0; i<4; i++) {
+//                 int nrow = row + delrow[i] ;
+//                 int ncol = col + delcol[i] ;
+                
+//                 if(nrow >=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0){
+//                     q.push({nrow, ncol}) ;
+//                     vis[nrow][ncol] = 1;
+//                 }
+//             }
+//         }
+        
+//         int count = 0 ;
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<m; j++) {
+//                 if(grid[i][j] == 1 && vis[i][j] == 0) {
+//                     count++ ;
+//                 }
+//             }
+//         }
+        
+//         return count;
+//     }
