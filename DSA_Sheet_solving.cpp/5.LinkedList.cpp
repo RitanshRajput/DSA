085584d67c5ae80ce15ac249831a7b42c0de1037
RitanshRajput <it2042@global.org.in>
2023-10-19 23:50:43 +0530

    day190

--- DSA_Sheet_solving.cpp/5.LinkedList.cpp
+++ DSA_Sheet_solving.cpp/5.LinkedList.cpp
@@ -2742,3 +2742,80 @@
 // }
 //🔸time complexity:  O(N+M)
 //🔸space complexity: O(N*M)         
+
+
+
+//              //❓Question: Delete nodes having greater right value on right node
+
+// Given a singly linked list, remove all the nodes in the list which have any node on their right whose value is greater.
+// (Not just immediate Right , but entire List on the Right)
+
+// Example 1:
+// Input:
+// LinkedList = 12->15->10->11->5->6->2->3
+// Output: 15 11 6 3
+// Explanation: Since, 12, 10, 5 and 2 are
+// the elements which have greater elements
+// on the following nodes. So, after deleting
+// them, the linked list would like be 15,
+// 11, 6, 3.
+
+// Example 2:
+// Input:
+// LinkedList = 10->20->30->40->50->60
+// Output: 60
+// Explanation: All the nodes except the last
+// node has a greater value node on its right,
+// so all the nodes except the last node must
+// be removed.
+// Your Task:
+// The task is to complete the function compute() which should modify the list as required and return the head of the modified linked list. 
+// The printing is done by the driver code,
+
+// Expected Time Complexity: O(N)
+// Expected Auxiliary Space: O(1)
+
+// Constraints:
+// 1 ≤ size of linked list ≤ 105
+// 1 ≤ element of linked list ≤ 105
+
+// Note: Try to solve the problem without using any extra space.
+
+//🔴approach : 
+// class Solution
+// {
+//     public:
+//     Node*rev(Node*cur){
+//         Node*c = cur;
+//         Node*pre = NULL;
+//         Node*n;
+//         while(c){
+//             n = c->next;
+//             c->next = pre;
+//             pre = c;
+//             c = n;
+//         }
+//         return pre;
+//     }
+//     Node *compute(Node *head)
+//     {
+        
+//         Node*recur = rev(head);
+//         Node*cur = recur;
+//         while(cur){
+//             if(cur->next && cur->data > cur->next->data){
+//                 Node*nn = cur->next->next;
+//                 Node*n = cur->next;
+//                 delete(n);
+//                 cur->next = nn;
+//             }
+//             else cur = cur->next;
+//         }
+//         Node*ans = rev(recur);
+//         return ans;
+//     }
+// };
+//🔸time complexity:  O(N)
+//🔸space complexity: O(1)
+
+
