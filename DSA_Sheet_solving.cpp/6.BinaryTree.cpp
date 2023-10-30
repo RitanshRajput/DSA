9b0714fc97453051d7c9bf128ca7c5af4a96df6b
RitanshRajput <it2042@global.org.in>
2023-10-30 11:27:47 +0530

    day193

--- DSA_Sheet_solving.cpp/6.BinaryTree.cpp
+++ DSA_Sheet_solving.cpp/6.BinaryTree.cpp
@@ -593,7 +593,6 @@
 //     }
 //     solve(ans, root->left, level+1) ;
 //     solve(ans, root->right, level+1) ;
-
 // }
 
 
@@ -819,3 +818,294 @@
 //🔸space complexity : O(2N)
 
 
+
+//                       //❓Question: Bottom View of Binary Tree
+
+// Given a binary tree, print the bottom view from left to right.
+// A node is included in bottom view if it can be seen when we look at the tree from bottom.
+
+//                       20
+//                     /    \
+//                   8       22
+//                 /   \        \
+//               5      3       25
+//                     /   \      
+//                   10    14
+
+// For the above tree, the bottom view is 5 10 3 14 25.
+// If there are multiple bottom-most nodes for a horizontal distance from root, then print the later one in level traversal. For example, in the below diagram, 3 and 4 are both the bottommost nodes at horizontal distance 0, we need to print 4.
+
+//                       20
+//                     /    \
+//                   8       22
+//                 /   \     /   \
+//               5      3  4     25
+//                     /    \      
+//                  10       14
+
+// For the above tree the output should be 5 10 4 14 25.
+
+// Note: The Input/Output format and Example given are used for the system's internal purpose, and should be used by a user for Expected Output only. As it is a function problem, hence a user should not read any input from the stdin/console. The task is to complete the function specified, and not to write the full code.
+
+// Example 1:
+// Input:
+//        1
+//      /   \
+//     3     2
+// Output: 3 1 2
+// Explanation:
+// First case represents a tree with 3 nodes
+// and 2 edges where root is 1, left child of
+// 1 is 3 and right child of 1 is 2.
+// Thus nodes of the binary tree will be printed as such 3 1 2.
+
+// Example 2:
+// Input:
+//          10
+//        /    \
+//       20    30
+//      /  \
+//     40   60
+// Output: 40 20 60 30
+// Your Task:
+// This is a functional problem, you don't need to care about input, just complete the function bottomView() which takes the root node of the tree as input and returns an array containing the bottom view of the given tree.
+
+// Expected Time Complexity: O(N*logN).
+// Expected Auxiliary Space: O(N).
+
+// Constraints:
+// 1 <= Number of nodes <= 105
+// 1 <= Data of a node <= 105
+
+
+//🔴appraoch : 
+//  vector <int> bottomView(Node *root) {
+        
+//         vector<int> ans;
+         
+//         if(root == NULL) return ans;
+        
+//         map<int,int> topNode;
+//         queue<pair<Node*,int>> q;
+//         q.push(make_pair(root,0));
+        
+        
+//         while(!q.empty()) {
+//             pair<Node*, int> temp = q.front();
+//             q.pop();
+            
+//             Node* frontNode = temp.first;
+//             int hd = temp.second;
+            
+//             topNode[hd] = frontNode->data;
+            
+//             if(frontNode->left){
+//                 q.push(make_pair(frontNode->left,hd-1));
+//             }
+            
+//             if(frontNode->right){
+//                 q.push(make_pair(frontNode->right,hd+1));
+//             }
+//         }
+        
+//         for(auto i : topNode){
+//             ans.push_back(i.second);
+//         }
+        
+//         return ans;
+//     }
+//🔸Time complexity : O(N * logN)
+//🔸space complexity : O(N)
+
+
+
+//                      //❓Question: ZigZag Traversal
+
+// Given a Binary Tree. Find the Zig-Zag Level Order Traversal of the Binary Tree.
+
+// Example 1:
+// Input:
+//         1
+//       /   \
+//      2     3
+//     / \   /  \
+//    4   5 6    7
+// Output:
+// 1 3 2 4 5 6 7
+
+// Example 2:
+// Input:
+//            7
+//         /     \
+//        9       7
+//      /  \     /   
+//     8    8   6     
+//    /  \
+//   10   9 
+// Output:
+// 7 7 9 8 8 6 9 10 
+
+// Your Task:
+// You don't need to read input or print anything. Your task is to complete the function zigZagTraversal() 
+// which takes the root node of the Binary Tree as its input and returns a list containing the node values 
+// as they appear in the Zig-Zag Level-Order Traversal of the Tree.
+
+// Expected Time Complexity: O(N).
+// Expected Auxiliary Space: O(N).
+
+// Constraints:
+// 1 <= N <= 104
+
+//🔴appraoch : 
+// int so(Node* &root ,vector<int> &v,vector<int> &ans)
+//     {
+//         queue<Node*> q;
+//         q.push(root);
+//         q.push(NULL);
+//         int i=0;
+//         while(!q.empty())
+//         {
+//             Node* t=q.front();
+//             q.pop();
+//             if(t==NULL)
+//             {
+//                 if(i==0)
+//                 {
+//                      for(int i:v)
+//                     {
+//                         ans.push_back(i);
+//                     }
+//                     v.clear();
+//                      i=1;
+//                 }
+//                 else
+//                 {
+//                     reverse(v.begin(),v.end());
+//                     for(int i:v)
+//                     {
+//                         ans.push_back(i);
+//                     }
+//                     v.clear();
+//                     i=0;
+
+//                 }
+//                 if(!q.empty())
+//                 {
+//                     q.push(NULL);
+//                 }
+//             }
+//             else
+//             {
+//                 v.push_back(t->data);
+//                 if(t->left!=NULL)
+//                 {
+//                     q.push(t->left);
+//                 }
+//                 if(t->right!=NULL)
+//                 {
+//                     q.push(t->right);
+//                 }
+
+//             }
+//         }
+//         return 1;
+
+//     }
+//     vector <int> zigZagTraversal(Node* root)
+//     {
+//          if(root==NULL)
+//         {
+//             return {};
+//         }
+//         vector<int> v;
+//         vector<int> ans;
+//         so(root,v,ans);
+//         return ans;
+//     }
+//🔸time complexity : O(N)
+//🔸space complexity: O(N)
+
+
+
+//                          //❓Question: check for Balanced Tree or Not
+
+// Given a binary tree, find if it is height balanced or not. 
+// A tree is height balanced if difference between heights of 
+// left and right subtrees is not more than one for all nodes of tree. 
+
+// A height balanced tree
+//         1
+//      /     \
+//    10      39
+//   /
+// 5
+
+// An unbalanced tree
+//         1
+//      /    
+//    10   
+//   /
+// 5
+
+// Example 1:
+
+// Input:
+//       1
+//     /
+//    2
+//     \
+//      3 
+// Output: 0
+// Explanation: The max difference in height
+// of left subtree and right subtree is 2,
+// which is greater than 1. Hence unbalanced
+// Example 2:
+
+// Input:
+//        10
+//      /   \
+//     20   30 
+//   /   \
+//  40   60
+// Output: 1
+// Explanation: The max difference in height
+// of left subtree and right subtree is 1.
+// Hence balanced. 
+// Your Task:
+// You don't need to take input. Just complete the function isBalanced() that takes root node as parameter and returns true, if the tree is balanced else returns false.
+
+// Constraints:
+// 1 <= Number of nodes <= 105
+// 1 <= Data of a node <= 109
+
+// Expected time complexity: O(N)
+// Expected auxiliary space: O(h) , where h = height of tree
+
+//🔴approach :
+//   int solve(Node* root,bool& ans)
+//     {
+//         if(root == NULL)
+//         {
+//             return 0;
+//         }
+        
+//         int left = solve(root->left,ans);
+//         int right = solve(root->right,ans);
+        
+//         int diff = left - right;
+//         if(abs(diff)>1)
+//         {
+//             ans = false;
+//         }
+//         return 1+max(left,right);
+//     }
+//     bool isBalanced(Node *root)
+//     {
+//         //  Your Code here
+//         bool ans = true;
+//         solve(root,ans);
+        
+//         return ans;
+//     }
+//🔸time complexity: O(N)
+//🔸space complexity: O(height of tree)
