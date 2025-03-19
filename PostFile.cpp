948e73616fe9e7ad7cdcf06a31e7f77f8e6bdd0a
RitanshRajput <ritanshrajput69@gmail.com>
2025-03-19 07:57:43 +0530

    Leetcode469

--- PostFile.cpp
+++ PostFile.cpp
@@ -1,1 +1,20 @@
-//
+class Solution {
+    public:
+        int minOperations(vector<int>& nums) {
+            int n=nums.size();
+            int c=0;
+            for(int i=0;i<n-1;i++){
+                if(nums[i]==0){
+                    if((i+2)>=n)return -1;
+                    nums[i]=!nums[i];
+                    nums[i+1]=!nums[i+1];
+                    nums[i+2]=!nums[i+2];
+                    c++;
+                }
+            }
+            for(int i=0;i<n;i++){
+                if(nums[i]==0)return -1;
+            }
+            return c;
+        }
+    };
